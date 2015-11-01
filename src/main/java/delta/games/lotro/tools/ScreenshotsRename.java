192413c46d9e2104fa0b7b5d9adab5e0b8139678
Damien Morcellet <dmorcellet@free.fr>
2015-11-01 18:26:12 +0000

    delta4j/lotro-tools:     * Added tool to rename screenshots with date instead of index

---
+++ src/main/java/delta/games/lotro/tools/ScreenshotsRename.java
@@ -0,0 +1,75 @@
+package delta.games.lotro.tools;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import delta.common.utils.files.iterator.FileIterator;
+import delta.common.utils.files.iterator.FileIteratorCallback;
+
+/**
+ * @author dm
+ */
+public class ScreenshotsRename
+{
+  private static final File ROOT_DIR=new File("X:\\damien\\docs\\jeux\\lotro\\screenshots");
+
+  private static final SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd-HHmmss");
+  private static void doRename(File f)
+  {
+    long time=f.lastModified();
+    String name=sdf.format(new Date(time))+".jpg";
+    File to=new File(f.getParentFile(),name);
+    if(!to.exists())
+    {
+      System.out.println("Rename to: "+name);
+      f.renameTo(to);
+    }
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    final FileFilter f=new FileFilter()
+    {
+      
+      public boolean accept(File pathname)
+      {
+        return pathname.getName().startsWith("ScreenShot");
+      }
+    };
+    FileIteratorCallback c=new FileIteratorCallback()
+    {
+      
+      public void leaveDirectory(File absolute, File relative)
+      {
+        // ToDo Auto-generated method stub
+        
+      }
+      
+      public void handleFile(File absolute, File relative)
+      {
+        if (f.accept(absolute))
+        {
+          doRename(absolute);
+        }
+      }
+
+      public void handleDirectory(File absolute, File relative)
+      {
+        // ToDo Auto-generated method stub
+      }
+      
+      public boolean enterDirectory(File absolute, File relative)
+      {
+        return true;
+      }
+    };
+
+    FileIterator it=new FileIterator(ROOT_DIR,true,c);
+    it.run();
+  }
+}
