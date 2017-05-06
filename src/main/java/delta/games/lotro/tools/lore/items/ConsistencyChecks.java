6a3fdd4d8a74d7e1ec53b6bbcbc256389dd4f287
Damien Morcellet <dmorcellet@free.fr>
2017-05-06 18:39:49 +0200

    ConsistencyChecks: added counters for scalable items and items with formulas

--- src/main/java/delta/games/lotro/tools/lore/items/ConsistencyChecks.java
+++ src/main/java/delta/games/lotro/tools/lore/items/ConsistencyChecks.java
@@ -8,6 +8,7 @@ import delta.games.lotro.lore.items.Armour;
 import delta.games.lotro.lore.items.ArmourType;
 import delta.games.lotro.lore.items.EquipmentLocation;
 import delta.games.lotro.lore.items.Item;
+import delta.games.lotro.lore.items.ItemPropertyNames;
 import delta.games.lotro.lore.items.Weapon;
 import delta.games.lotro.lore.items.WeaponType;
 import delta.games.lotro.lore.items.legendary.Legendary;
@@ -18,6 +19,7 @@ import delta.games.lotro.lore.items.legendary.Legendary;
  */
 public class ConsistencyChecks
 {
+  private List<Item> _scalableItems;
   private List<Item> _missingStats;
   private int _nbMissingStats;
   private int _nbStats;
@@ -29,6 +31,7 @@ public class ConsistencyChecks
   public ConsistencyChecks()
   {
     _missingStats=new ArrayList<Item>();
+    _scalableItems=new ArrayList<Item>();
   }
 
   /**
@@ -40,6 +43,8 @@ public class ConsistencyChecks
     int nbMissingArmourValues=0;
     int nbMissingArmourTypes=0;
     int nbMissingWeaponTypes=0;
+    int nbScalables=0;
+    int nbFormulas=0;
     for(Item item : items)
     {
       checkItemStats(item);
@@ -73,6 +78,27 @@ public class ConsistencyChecks
           //System.out.println("No weapon type for: " + name + " (" + id + ')');
         }
       }
+      // Scalables
+      String itemLevels=item.getProperty("itemLevels");
+      String slicedStats=item.getProperty(ItemPropertyNames.SLICED_STATS);
+      if (slicedStats!=null)
+      {
+        nbFormulas++;
+        if (itemLevels!=null)
+        {
+          nbScalables++;
+          _scalableItems.add(item);
+          if (item instanceof Armour)
+          {
+            Armour armour=(Armour)item;
+            ArmourType type=armour.getArmourType();
+            if (type==null)
+            {
+              System.out.println("No armour type for: " + item + " with formula: "+slicedStats);
+            }
+          }
+        }
+      }
     }
     System.out.println("Nb armours with missing armour type: " + nbMissingArmourTypes);
     System.out.println("Nb armours with missing armour value: " + nbMissingArmourValues);
@@ -80,7 +106,9 @@ public class ConsistencyChecks
     System.out.println("Nb legendary items: " + _nbLegendaryItems);
     System.out.println("Nb items with stats: " + _nbStats);
     System.out.println("Nb items with missing stats: " + _nbMissingStats);
-    new ItemStatistics().showStatistics(_missingStats);
+    System.out.println("Nb items with formula: " + nbFormulas);
+    System.out.println("Nb scalable items: " + nbScalables);
+    //new ItemStatistics().showStatistics(_scalableItems);
   }
 
   private void checkItemStats(Item item) 
