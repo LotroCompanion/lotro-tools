d914fa66bccbe1c1667b58e08391a2330f07a09a
Damien Morcellet <dmorcellet@free.fr>
2017-10-27 17:26:58 +0200

    Attempt to resolve item references in the deeds database

--- src/main/java/delta/games/lotro/tools/lore/deeds/DeedsItemsLoader.java
+++ src/main/java/delta/games/lotro/tools/lore/deeds/DeedsItemsLoader.java
@@ -12,6 +12,8 @@ import delta.games.lotro.lore.deeds.DeedsManager;
 import delta.games.lotro.lore.deeds.index.DeedCategory;
 import delta.games.lotro.lore.deeds.index.DeedSummary;
 import delta.games.lotro.lore.deeds.index.DeedsIndex;
+import delta.games.lotro.lore.items.Item;
+import delta.games.lotro.tools.lore.items.ItemsResolver;
 import delta.games.lotro.utils.LotroLoggers;
 
 /**
@@ -22,7 +24,20 @@ public class DeedsItemsLoader
 {
   private static final Logger _logger=LotroLoggers.getWebInputLogger();
 
+  private ItemsResolver _resolver;
+
+  private int _resolved=0;
+  private int _missed=0;
+
   private static final String WIKI_SEED="/wiki/";
+
+  /**
+   * Constructor.
+   */
+  public DeedsItemsLoader()
+  {
+    _resolver=new ItemsResolver();
+  }
 
   private void handleSet(ObjectsSet set)
   {
@@ -32,15 +47,65 @@ public class DeedsItemsLoader
       for(int i=0;i<nb;i++)
       {
         ObjectItem item=set.getItem(i);
-        String url=item.getObjectURL();
-        if ((url!=null) && (url.startsWith(WIKI_SEED)))
-        {
-          String itemId=url.substring(WIKI_SEED.length());
-          System.out.println("ItemID: "+itemId);
-        }
-        String icon=item.getIconURL();
-        System.out.println("Icon: "+icon);
+        handleItem(item);
       }
+    }
+  }
+
+  private void handleItem(ObjectItem itemReference)
+  {
+    // Use name
+    String name=itemReference.getName();
+    Item item=_resolver.getItem(name);
+    if (item==null)
+    {
+      // Use item 'key' (Wiki key)
+      String url=itemReference.getObjectURL();
+      if ((url!=null) && (url.startsWith(WIKI_SEED)))
+      {
+        String itemKey=url.substring(WIKI_SEED.length());
+        item=_resolver.getItem(itemKey);
+      }
+    }
+    if (item==null)
+    {
+      // Use icon path
+      String iconUrl=itemReference.getIconURL();
+      if (iconUrl!=null)
+      {
+        iconUrl=ItemsResolver.normalizeIconUrl(iconUrl);
+        item=_resolver.getItem(iconUrl);
+      }
+    }
+    if (item!=null)
+    {
+      // Found!
+      itemReference.setItemId(item.getIdentifier());
+      itemReference.setIconURL(null);
+      itemReference.setObjectURL(null);
+      _resolved++;
+    }
+    else if ("Rotten Fruit".equals(name))
+    {
+      // Found!
+      itemReference.setItemId(1879199969);
+      itemReference.setIconURL(null);
+      itemReference.setObjectURL(null);
+      _resolved++;
+    }
+    else if ("Flower Petals".equals(name))
+    {
+      // Found!
+      itemReference.setItemId(1879199971);
+      itemReference.setIconURL(null);
+      itemReference.setObjectURL(null);
+      _resolved++;
+    }
+    else
+    {
+      // Not found!
+      System.out.println("Not found: "+itemReference);
+      _missed++;
     }
   }
 
@@ -83,6 +148,7 @@ public class DeedsItemsLoader
           indexDeed++;
         }
       }
+      System.out.println("Resolved: "+_resolved+", missed: "+_missed);
     }
     else
     {
