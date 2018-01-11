f67b961ae3a1fac15287b13a1dc41cebd72fef6f
Damien Morcellet <dmorcellet@free.fr>
2018-01-11 22:26:27 +0100

    Removed obsolete deeds-related tooling ; added deeds file builder

---
+++ src/main/java/delta/games/lotro/tools/lore/deeds/DeedsFileBuilder.java
@@ -0,0 +1,43 @@
+package delta.games.lotro.tools.lore.deeds;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import delta.common.utils.text.EncodingNames;
+import delta.games.lotro.LotroCoreConfig;
+import delta.games.lotro.lore.deeds.DeedDescription;
+import delta.games.lotro.lore.deeds.io.xml.DeedXMLParser;
+import delta.games.lotro.lore.deeds.io.xml.DeedXMLWriter;
+
+/**
+ * Builds a single deeds file from a collection of deed files.
+ * @author DAM
+ */
+public class DeedsFileBuilder
+{
+  private void doIt()
+  {
+    File loreDir=LotroCoreConfig.getInstance().getLoreDir();
+    File deedsDir=new File(loreDir,"deeds");
+    List<DeedDescription> deeds=new ArrayList<DeedDescription>();
+    DeedXMLParser parser=new DeedXMLParser();
+    for(File deedFile : deedsDir.listFiles())
+    {
+      List<DeedDescription> newDeeds=parser.parseXML(deedFile);
+      deeds.addAll(newDeeds);
+    }
+    File out=new File(loreDir,"deeds.xml");
+    DeedXMLWriter writer=new DeedXMLWriter();
+    writer.writeDeeds(out,deeds,EncodingNames.UTF_8);
+  }
+
+  /**
+   * Main method for this tool.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    new DeedsFileBuilder().doIt();
+  }
+}
