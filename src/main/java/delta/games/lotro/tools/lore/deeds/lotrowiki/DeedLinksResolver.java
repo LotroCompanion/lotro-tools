4d99675ec9e3f4d7ea5b396c3ea1a8f128cb16a3
Damien Morcellet <dmorcellet@free.fr>
2018-02-02 11:19:03 +0100

    lotro-wiki deeds parser: automagically add missing 'advanced' skirmish lieutenant slayer deeds

--- src/main/java/delta/games/lotro/tools/lore/deeds/lotrowiki/DeedLinksResolver.java
+++ src/main/java/delta/games/lotro/tools/lore/deeds/lotrowiki/DeedLinksResolver.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import delta.games.lotro.common.objects.ObjectItem;
 import delta.games.lotro.lore.deeds.DeedDescription;
 import delta.games.lotro.lore.deeds.DeedProxy;
+import delta.games.lotro.lore.deeds.DeedType;
 
 /**
  * Resolver for deed links.
@@ -14,6 +16,7 @@ import delta.games.lotro.lore.deeds.DeedProxy;
 public class DeedLinksResolver
 {
   private List<DeedDescription> _deeds;
+  private List<DeedDescription> _toAdd;
   private HashMap<String,DeedDescription> _mapByName;
 
   /**
@@ -22,7 +25,8 @@ public class DeedLinksResolver
    */
   public DeedLinksResolver(List<DeedDescription> deeds)
   {
-    _deeds=new ArrayList<DeedDescription>(deeds);
+    _deeds=deeds;
+    _toAdd=new ArrayList<DeedDescription>();
     loadMap();
   }
 
@@ -49,6 +53,7 @@ public class DeedLinksResolver
     {
       resolveDeed(deed);
     }
+    _deeds.addAll(_toAdd);
   }
 
   private void resolveDeed(DeedDescription deed)
@@ -76,7 +81,59 @@ public class DeedLinksResolver
     }
     else
     {
-      System.out.println("Deed not found: "+deedName);
+      boolean ok=false;
+      if (deedName.endsWith(" Slayer (Advanced)"))
+      {
+        String baseDeedName=deedName.substring(0, deedName.length() - 10).trim();
+        DeedDescription baseDeed=_mapByName.get(baseDeedName);
+        if (baseDeed != null)
+        {
+          DeedDescription advancedDeed=buildAdvancedSkirmishLieutenantDeed(baseDeed);
+          _toAdd.add(advancedDeed);
+          ok=true;
+        }
+      }
+      if (!ok)
+      {
+        System.out.println("Deed not found: "+deedName);
+      }
     }
+  }
+
+  private DeedDescription buildAdvancedSkirmishLieutenantDeed(DeedDescription baseDeed) {
+    DeedDescription deed = new DeedDescription();
+    deed.setKey(baseDeed.getKey()+"_(Advanced)");
+    deed.setName(baseDeed.getName()+" (Advanced)");
+    deed.setType(DeedType.SLAYER);
+    deed.setCategory(baseDeed.getCategory());
+    deed.setDescription(baseDeed.getDescription());
+    // Objectives
+    // First line of base deed, replace (5) by (50)
+    {
+      String newObjectives="";
+      String objectives=baseDeed.getObjectives();
+      int index=objectives.indexOf('\n');
+      if (index!=-1)
+      {
+        newObjectives=objectives.substring(0,index);
+      }
+      else
+      {
+        newObjectives=objectives;
+      }
+      newObjectives=newObjectives.replace("(5)","(50)");
+      deed.setObjectives(newObjectives);
+    }
+    // Previous
+    DeedProxy previous=new DeedProxy();
+    previous.setDeed(baseDeed);
+    previous.setKey(previous.getKey());
+    previous.setName(previous.getName());
+    deed.setPreviousDeedProxy(previous);
+    // Rewards
+    ObjectItem item=new ObjectItem("Mark");
+    item.setItemId(1879224343);
+    deed.getRewards().getObjects().addObject(item,500);
+    return deed;
   }
 }
