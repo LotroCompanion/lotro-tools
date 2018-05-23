7f5fb6773485340b5d28f69927a557cee7172b81
Damien Morcellet <dmorcellet@free.fr>
2018-05-23 23:15:43 +0200

    Added some 'factory comments' for some Felegoth barter items

--- src/main/java/delta/games/lotro/tools/lore/items/FactoryCommentsInjector.java
+++ src/main/java/delta/games/lotro/tools/lore/items/FactoryCommentsInjector.java
@@ -66,6 +66,109 @@ public class FactoryCommentsInjector
   {
     //String comment="Recipe Kindred Erebor: single use, 40 tokens of lake and river";
     doDale();
+    doFelegoth();
+  }
+
+  private void doFelegoth()
+  {
+    String comment=null;
+    // Barter Felegoth
+    // - Kindred
+    {
+      comment="Barter Kindred Felegoth: 90 tokens of lake and river";
+      // - ears (agility)
+      // 1879347747 Opal Gold Earring of Thranduil's Power
+      // 1879347644 Rhodalite Silver Earring of Thranduil's Power
+      // 1879347651 Opal Gold Earring of Thranduil's Cunning
+      // 1879347741 Rhodalite Silver Earring of Thranduil's Cunning
+      int[] kindredAgility=new int[]{ 1879347747, 1879347644, 1879347651, 1879347741 };
+      injectComment(comment,kindredAgility);
+      injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,kindredAgility);
+      // - ears (might)
+      // 1879347690 Amethyst Iron Earring of Thranduil's Cunning
+      // 1879347710 Fire Opal Steel Earring of Thranduil's Power
+      // 1879347740 Fire Opal Steel Earring of Thranduil's Cunning
+      // 1879347743 Amethyst Iron Earring of Thranduil's Power
+      int[] kindredMight=new int[]{ 1879347690, 1879347710, 1879347740, 1879347743 };
+      injectComment(comment,kindredMight);
+      injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,kindredMight);
+    }
+    // - Ally
+    {
+      comment="Barter Ally Felegoth: 60 tokens of lake and river";
+      // - neck/pocket agi
+      // 1879347655 Stylish Pendant of Thranduil's Power
+      // 1879347751 Stylish Pendant of Thranduil's Cunning
+      // 1879347698 Meaningful Locket of Thranduil's Cunning
+      // 1879347738 Meaningful Locket of Thranduil's Power
+      int[] allyAgility=new int[]{ 1879347655, 1879347751, 1879347698, 1879347738 };
+      injectComment(comment,allyAgility);
+      injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,allyAgility);
+      // - neck/pocket might
+      // 1879347626 Heavy Choker of Thranduil's Cunning
+      // 1879347750 Heavy Choker of Thranduil's Power
+      // 1879347633 Antique Brooch of Thranduil's Cunning
+      // 1879347726 Antique Brooch of Thranduil's Power
+      int[] allyMight=new int[]{ 1879347626, 1879347750, 1879347633, 1879347726 };
+      injectComment(comment,allyMight);
+      injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,allyMight);
+    }
+    // - Friend
+    {
+      comment="Barter Friend Felegoth: 55 tokens of lake and river";
+      // Agility
+      {
+        // - wrist
+        // 1879347689 Agile Silver Bracelet of Thranduil's Power
+        // 1879347709 Agile Silver Bracelet of Thranduil's Cunning
+        // 1879347739 Dextrous Gold Bracelet of Thranduil's Power
+        // 1879347744 Dextrous Gold Bracelet of Thranduil's Cunning
+        int[] friendWrist=new int[]{ 1879347689, 1879347709, 1879347739, 1879347744 };
+        injectComment(comment,friendWrist);
+        injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,friendWrist);
+        // - ear
+        // 1879347635 Tourmaline Silver Earring of Thranduil's Cunning
+        // 1879347663 Moonstone Gold Earring of Thranduil's Cunning
+        // 1879347673 Moonstone Gold Earring of Thranduil's Power
+        // 1879347717 Tourmaline Silver Earring of Thranduil's Power
+        int[] friendEar=new int[]{ 1879347635, 1879347663, 1879347673, 1879347717 };
+        injectComment(comment,friendEar);
+        injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,friendEar);
+      }
+      // Might
+      {
+        // -wrist
+        // 1879347667 Strong Steel Bracelet of Thranduil's Power
+        // 1879347669 Strong Steel Bracelet of Thranduil's Cunning
+        // 1879347730 Tough Iron Bracelet of Thranduil's Power
+        // 1879347734 Tough Iron Bracelet of Thranduil's Cunning
+        int[] friendWrist=new int[]{ 1879347667, 1879347669, 1879347730, 1879347734 };
+        injectComment(comment,friendWrist);
+        injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,friendWrist);
+        // -ear
+        // 1879347625 Topaz Steel Earring of Thranduil's Cunning
+        // 1879347630 Topaz Steel Earring of Thranduil's Power
+        // 1879347672 Lapis Iron Earring of Thranduil's Power
+        // 1879347724 Lapis Iron Earring of Thranduil's Cunning
+        int[] friendEar=new int[]{ 1879347625, 1879347630, 1879347672, 1879347724 };
+        injectComment(comment,friendEar);
+        injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,friendEar);
+      }
+    }
+    // - Acquaintance
+    {
+      comment="Barter Acquaintance Felegoth: 55 tokens of lake and river";
+      // - ring
+      /*
+      // 1879347732 Carved Jade Ring of Bard's Honour
+      // 1879347711 Curious Onyx Ring of Bard's Honour
+      // 1879347652 Carved Jade Ring of Bard's Will
+      // 1879347681 Curious Onyx Ring of Bard's Will
+      int[] acquaintanceRing=new int[]{ 1879347732, 1879347711, 1879347652, 1879347681 };
+      injectComment(comment,acquaintanceRing);
+      injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,acquaintanceRing);
+      */
+    }
   }
 
   private void doDale()
@@ -101,10 +204,10 @@ public class FactoryCommentsInjector
     {
       comment="Barter Friend Dale: 55 tokens of lake and river";
       // - wrist
-      // 1879347670" Delicate Gold Armlet of Bard's Will
-      // 1879347745" Woven Silver Armlet of Bard's Will
-      // 1879347676" Delicate Gold Armlet of Bard's Honour
-      // 1879347648" Woven Silver Armlet of Bard's Honour
+      // 1879347670 Delicate Gold Armlet of Bard's Will
+      // 1879347745 Woven Silver Armlet of Bard's Will
+      // 1879347676 Delicate Gold Armlet of Bard's Honour
+      // 1879347648 Woven Silver Armlet of Bard's Honour
       int[] friendWrist=new int[]{ 1879347670, 1879347745, 1879347676, 1879347648 };
       injectComment(comment,friendWrist);
       injectBinding(ItemBinding.BOUND_TO_ACCOUNT_ON_ACQUIRE,friendWrist);
