5edeb0fe7250e86748229023d7df1abd279bfab6
Damien Morcellet <dmorcellet@free.fr>
2018-12-17 18:17:55 +0100

    Updated MainDatBrowser

--- src/main/java/delta/games/lotro/tools/dat/utils/MainDatBrowser.java
+++ src/main/java/delta/games/lotro/tools/dat/utils/MainDatBrowser.java
@@ -1,4 +1,4 @@
-package delta.games.lotro.tools.dat;
+package delta.games.lotro.tools.dat.utils;
 
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
@@ -7,11 +7,11 @@ import java.util.List;
 import java.util.Map;
 
 import delta.common.utils.misc.IntegerHolder;
-import delta.games.lotro.dat.WStateClass;
 import delta.games.lotro.dat.data.DataFacade;
 import delta.games.lotro.dat.data.PropertiesSet;
 import delta.games.lotro.dat.utils.BufferUtils;
 import delta.games.lotro.dat.utils.KPM;
+import delta.games.lotro.utils.maths.Progression;
 
 /**
  * @author DAM
@@ -45,11 +45,13 @@ public class MainDatBrowser
     _countByType=new HashMap<Integer,IntegerHolder>();
   }
 
+  /*
   private static int[] IDS= {1879049908,1879059895,1879060264,1879084796,1879084798,1879084800,1879085263,1879085275,1879085288,1879085289,1879102770,
       1879105669,1879105702,1879105719,1879105751,1879105764,1879105766,1879105808,1879105875,1879105891,1879105894,1879105901,1879105921,1879106009,
       1879106037,1879106052,1879106072,1879106073,1879106093,1879106104,1879106124,1879106127,1879106147,1879106213,1879106249,1879119370,1879136185,
       1879151960,1879156819,1879157300,1879189153,1879189154,1879189155,1879189156,1879189157,1879192023,1879211576,1879211633,1879212135,1879220100,
       1879222091,1879222092,1879224296,1879232939,1879239108,1879267320,1879325961,1879381487};
+      */
 
   private List<Integer> ids=new ArrayList<Integer>();
 
@@ -60,15 +62,8 @@ public class MainDatBrowser
     {
       int type=getType(data);
       Integer typeKey=Integer.valueOf(type);
-      // Count by type
-      IntegerHolder holder=_countByType.get(typeKey);
-      if (holder==null)
-      {
-        holder=new IntegerHolder();
-        _countByType.put(typeKey,holder);
-      }
-      holder.increment();
       // Select
+      /*
       if ((type!=WStateClass.CLOTHING) && (type!=WStateClass.CHISEL) && (type!=WStateClass.SHIELD) && (type!=WStateClass.WEAPON) && (type!=WStateClass.JEWEL) &&
           (type!=WStateClass.PROGRESSION) && (type!=WStateClass.PROGRESSION_ARRAY) && (type!=WStateClass.FLOAT_PROGRESSION_ARRAY) && 
           (type!=WStateClass.QUEST) && (type!=WStateClass.RELIC) &&
@@ -78,15 +73,33 @@ public class MainDatBrowser
           (type!=WStateClass.QUEST_TREASURE) && (type!=WStateClass.LOOT) && (type!=WStateClass.LOOT2) && (type!=WStateClass.SKIRMISH_LOOT) &&
           (type!=WStateClass.SET) && (type!=WStateClass.IA_LEGACY) && (type!=WStateClass.IA_EFFECT) &&
           (type!=WStateClass.WEB_STORE_ITEM))
+          */
       {
         int index=findBuffer(data,_toSearch);
         if (index!=-1)
         {
+          // Count by type
+          IntegerHolder holder=_countByType.get(typeKey);
+          if (holder==null)
+          {
+            holder=new IntegerHolder();
+            _countByType.put(typeKey,holder);
+          }
+          holder.increment();
           System.out.println("Found "+id+" index="+index+", type="+type);
           PropertiesSet props=_facade.loadProperties(id+0x9000000);
           System.out.println("*********** entry "+id+"******************");
           System.out.println(props.dump());
           ids.add(Integer.valueOf(id));
+          Progression prog=ProgressionFactory.buildProgression(id,props);
+          if (prog!=null)
+          {
+            Float value=prog.getValue(120);
+            if ((value!=null) && (Math.abs(value.floatValue()-3)<0.1))
+            {
+              System.out.println(props.dump());
+            }
+          }
         }
       }
     }
@@ -123,10 +136,19 @@ public class MainDatBrowser
 
   private void doIt()
   {
+    //searchId(268435801); // Vital_PowerCombatRegenAddMod
+    //searchId(268438584); // Vital_HealthCombatRegenAddMod
+    //searchId(268439070); // Vital_HealthCombatBaseRegen
+    //searchId(268445007); // Trait_MP_Virtue_Core_Rank_Health_Regen
+    //searchId(268437690); // Health_RegenRate
+    searchId(268435576); // Vital_HealthCombatCurrentRegen
+
+    /*
     for(int id: IDS)
     {
       searchId(id);
     }
+    */
     System.out.println(_countByType);
     System.out.println(ids);
   }
