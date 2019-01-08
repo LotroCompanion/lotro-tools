0aae04ae05b2d8cd888ac4f1a6f58d029953ef6f
Damien Morcellet <dmorcellet@free.fr>
2019-01-08 16:02:33 +0100

    Extract vendor data for NPCs

--- src/main/java/delta/games/lotro/tools/dat/npc/MainDatNpcLoader.java
+++ src/main/java/delta/games/lotro/tools/dat/npc/MainDatNpcLoader.java
@@ -1,7 +1,11 @@
 package delta.games.lotro.tools.dat.npc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -12,6 +16,7 @@ import delta.games.lotro.dat.data.enums.EnumMapper;
 import delta.games.lotro.dat.utils.BufferUtils;
 import delta.games.lotro.lore.items.Item;
 import delta.games.lotro.lore.items.ItemsManager;
+import delta.games.lotro.lore.items.comparators.ItemNameComparator;
 import delta.games.lotro.lore.items.io.xml.ItemSaxParser;
 import delta.games.lotro.tools.dat.GeneratedFiles;
 import delta.games.lotro.tools.dat.utils.DatUtils;
@@ -27,6 +32,8 @@ public class MainDatNpcLoader
   private DataFacade _facade;
   private ItemsManager _itemsManager;
   private EnumMapper _characterClass;
+  private Set<Integer> _profileIds=new HashSet<Integer>();
+  private Map<Integer,Set<Integer>> _sellsListIds=new HashMap<Integer,Set<Integer>>();
 
   /**
    * Constructor.
@@ -64,27 +71,18 @@ public class MainDatNpcLoader
         loadClassRequirement(properties);
         loadReputationRequirement(properties);
         loadQuestRequirements(properties);
-        // Profiles
+        // Barter profiles
         for(Object barterProfileObj : barterProfiles)
         {
           int profileId=((Integer)barterProfileObj).intValue();
           loadBarterProfile(indexDataId,profileId);
         }
-
+        // Vendor
+        loadVendorData(properties);
+        // TODO:
         /*
-        Usage_RequiredClassList: 
-          #1: 162
-        Vendor_BuysItems: 1
-        Vendor_DiscountList: 
-          #1: 268450539
-        Vendor_InventoryList: 
-          #1: 1879052429
-        Vendor_ItemList: 
-          #1: 1879205686
-          #2: 1879052507
-          #3: 1879143219
-        Vendor_SellModifier: 4.0
-        Vendor_SellsWearableItems: 0
+        Barter_ItemUsesMinUsageLevelForCost
+        WorldEvent_WorldEvent: 1879286443
         */
         //System.out.println(properties.dump());
       }
@@ -139,15 +137,13 @@ public class MainDatNpcLoader
     }
   }
 
-  private Set<Integer> profileIds=new HashSet<Integer>();
-
   private void loadBarterProfile(int npcId,int profileId)
   {
-    if (profileIds.contains(Integer.valueOf(profileId)))
+    if (_profileIds.contains(Integer.valueOf(profileId)))
     {
       return;
     }
-    profileIds.add(Integer.valueOf(profileId));
+    _profileIds.add(Integer.valueOf(profileId));
     int dbPropertiesId=profileId+0x09000000;
     PropertiesSet properties=_facade.loadProperties(dbPropertiesId);
     if (npcId==0)
@@ -237,6 +233,134 @@ public class MainDatNpcLoader
       System.out.println(itemProps.dump());
     }
     return label;
+  }
+
+  private void loadVendorData(PropertiesSet properties)
+  {
+    // Sells lists
+    Set<Integer> allSells=new HashSet<Integer>();
+    Object[] inventoryArray=(Object[])properties.getProperty("Vendor_InventoryList");
+    if (inventoryArray!=null)
+    {
+      for(Object inventoryObj : inventoryArray)
+      {
+        int itemId=((Integer)inventoryObj).intValue();
+        Set<Integer> itemsInSellsList=loadSellsList(itemId);
+        allSells.addAll(itemsInSellsList);
+      }
+    }
+    // Item sells
+    Set<Integer> itemSells=loadItemSells(properties);
+    Set<Integer> newItemSells=new HashSet<Integer>();
+    for(Integer itemSell : itemSells)
+    {
+      if (!allSells.contains(itemSell))
+      {
+        System.out.println("Found new item in item sells: "+itemSell);
+        newItemSells.add(itemSell);
+      }
+    }
+    int nbItemSells=itemSells.size();
+    int nbAllSells=allSells.size();
+    if ((nbItemSells!=0) && (nbItemSells!=nbAllSells))
+    {
+      System.out.println("Mismatch sells="+nbItemSells+" all sells="+nbAllSells);
+    }
+    if (newItemSells.size()>0)
+    {
+      System.out.println("Additional sells:");
+      displayItems(newItemSells);
+    }
+    // Buys items?
+    Integer buysItemsInt=(Integer)properties.getProperty("Vendor_BuysItems");
+    boolean buysItems=(buysItemsInt!=null)?(buysItemsInt.intValue()==1):false;
+    if (buysItems)
+    {
+      System.out.println("\tBuys items");
+    }
+    // Discount list
+    Object[] discountArray=(Object[])properties.getProperty("Vendor_DiscountList");
+    if (discountArray!=null)
+    {
+      for(Object discountIdObj : discountArray)
+      {
+        int discountId=((Integer)discountIdObj).intValue();
+        System.out.println("\tDiscount ID: "+discountId);
+      }
+    }
+    // Buys items?
+    Integer sellsWearableItemsInt=(Integer)properties.getProperty("Vendor_SellsWearableItems");
+    boolean sellsWearableItems=(sellsWearableItemsInt!=null)?(sellsWearableItemsInt.intValue()==1):false;
+    if (sellsWearableItems)
+    {
+      System.out.println("\tSells wearable items");
+    }
+    // Sell modifier
+    Float sellFactor=(Float)properties.getProperty("Vendor_SellModifier");
+    if (sellFactor!=null)
+    {
+      System.out.println("\tSells factor: "+sellFactor);
+    }
+  }
+
+  private Set<Integer> loadItemSells(PropertiesSet properties)
+  {
+    Set<Integer> set=new HashSet<Integer>();
+    Object[] itemsList=(Object[])properties.getProperty("Vendor_ItemList");
+    if (itemsList!=null)
+    {
+      for(Object itemIdObj : itemsList)
+      {
+        Integer itemId=(Integer)itemIdObj;
+        set.add(itemId);
+      }
+    }
+    return set;
+  }
+
+  private Set<Integer> loadSellsList(int listId)
+  {
+    Set<Integer> set=_sellsListIds.get(Integer.valueOf(listId));
+    if (set!=null)
+    {
+      return set;
+    }
+    set=new HashSet<Integer>();
+    _sellsListIds.put(Integer.valueOf(listId),set);
+    PropertiesSet properties=_facade.loadProperties(listId+0x09000000);
+    Object[] inventoryArray=(Object[])properties.getProperty("VendorInventory_Items");
+    if (inventoryArray!=null)
+    {
+      System.out.println("Sells list ID="+listId);
+      for(Object inventoryObj : inventoryArray)
+      {
+        Integer itemId=(Integer)inventoryObj;
+        set.add(itemId);
+      }
+      displayItems(set);
+    }
+    return set;
+  }
+
+  private void displayItems(Set<Integer> ids)
+  {
+    List<Item> items=new ArrayList<Item>();
+    for(Integer id : ids)
+    {
+      Item item=_itemsManager.getItem(id.intValue());
+      if (item!=null)
+      {
+        items.add(item);
+      }
+    }
+    Collections.sort(items,new ItemNameComparator());
+    int index=1;
+    for(Item item : items)
+    {
+      String itemName=item.getName();
+      System.out.println("\t#"+index+": "+itemName);
+      index++;
+    }
   }
 
   private void doIt()
