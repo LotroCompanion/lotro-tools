b8e4cf70be6d6fef6f3c3e273d9c9edaadd9165f
Damien Morcellet <dmorcellet@free.fr>
2019-01-20 18:50:49 +0100

    Flush code of the legacies loader

--- src/main/java/delta/games/lotro/tools/dat/items/legendary/MainDatLegaciesLoader.java
+++ src/main/java/delta/games/lotro/tools/dat/items/legendary/MainDatLegaciesLoader.java
@@ -1,15 +1,26 @@
 package delta.games.lotro.tools.dat.items.legendary;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import delta.games.lotro.common.stats.StatDescription;
+import delta.games.lotro.common.stats.StatsProvider;
+import delta.games.lotro.common.stats.StatsRegistry;
 import delta.games.lotro.dat.data.DataFacade;
 import delta.games.lotro.dat.data.PropertiesSet;
 import delta.games.lotro.dat.data.PropertyDefinition;
+import delta.games.lotro.lore.items.legendary.Legacy;
+import delta.games.lotro.lore.items.legendary.LegacyType;
 import delta.games.lotro.tools.dat.utils.DatEffectUtils;
+import delta.games.lotro.tools.dat.utils.DatStatUtils;
+import delta.games.lotro.utils.maths.ArrayProgression;
+import delta.games.lotro.utils.maths.Progression;
 
 /**
- * Get legendary data from DAT files.
+ * Get legacy descriptions from DAT files.
  * @author DAM
  */
-public class MainDatLegendariesExplorer
+public class MainDatLegaciesLoader
 {
   private DataFacade _facade;
 
@@ -17,22 +28,53 @@ public class MainDatLegendariesExplorer
    * Constructor.
    * @param facade Data facade.
    */
-  public MainDatLegendariesExplorer(DataFacade facade)
+  public MainDatLegaciesLoader(DataFacade facade)
   {
     _facade=facade;
   }
 
-  private void loadLegacy(int id)
+  private Set<String> statNames=new HashSet<String>();
+
+  private Legacy loadLegacy(int id)
   {
     System.out.println("**** ID="+id+" *****");
     PropertiesSet props=_facade.loadProperties(id+0x9000000);
     //System.out.println(props.dump());
 
+    statNames.addAll(props.getPropertyNames());
+    Legacy ret=new Legacy();
+    // ID
+    ret.setId(id);
+    // Max tier
+    int maxTier=((Integer)props.getProperty("ItemAdvancement_AdvanceableWidget_AbsoluteMaxLevel")).intValue();
+    ret.setMaxTier(maxTier);
+    // Category
+    Integer categoryCode=(Integer)props.getProperty("ItemAdvancement_AdvanceableWidget_Category");
+    if (categoryCode!=null)
+    {
+      LegacyType type=getLegacyType(categoryCode.intValue());
+      ret.setType(type);
+    }
+    // DPS LUT
+    Integer dpsLut=(Integer)props.getProperty("ItemAdvancement_AdvanceableWidget_DPSLUT");
+    if (dpsLut!=null)
+    {
+      loadDpsLut(dpsLut.intValue());
+    }
+
+    // Initial max tier
+    int initialMaxTier=((Integer)props.getProperty("ItemAdvancement_AdvanceableWidget_InitialMaxLevel")).intValue();
+    ret.setMaxInitialLevel(initialMaxTier);
+    // Stats
     Integer imbuedEffect=(Integer)props.getProperty("ItemAdvancement_ImbuedLegacy_Effect");
     if (imbuedEffect!=null)
     {
-      DatEffectUtils.loadEffect(_facade,imbuedEffect.intValue());
+      StatsProvider provider=DatEffectUtils.loadEffect(_facade,imbuedEffect.intValue());
+      ret.setStatsProvider(provider);
     }
+
+    System.out.println(ret);
+
     PropertiesSet mutationProps=(PropertiesSet)props.getProperty("ItemAdvancement_ImbuedLegacy_ClassicLegacyTransform");
     if (mutationProps!=null)
     {
@@ -41,38 +83,77 @@ public class MainDatLegendariesExplorer
       {
         int oldLegacy=((Integer)oldStatObj).intValue();
         PropertyDefinition propDef=_facade.getPropertiesRegistry().getPropertyDef(oldLegacy);
-        System.out.println("Old legacy: "+propDef.getName());
+        StatsRegistry stats=StatsRegistry.getInstance();
+        StatDescription oldStat=stats.getByKey(propDef.getName());
+        System.out.println("Old stat: "+oldStat.getName());
       }
     }
     else
     {
       System.out.println("No mutation data");
     }
+    return ret;
+  }
+
+  private void loadDpsLut(int id)
+  {
+    Progression prog=DatStatUtils.getProgression(_facade,id);
+    System.out.println("Nb points: "+((ArrayProgression)prog).getNumberOfPoints());
+    System.out.println("ID: "+((ArrayProgression)prog).getIdentifier());
+  }
+
+  private LegacyType getLegacyType(int categoryCode)
+  {
+    if (categoryCode==1) return LegacyType.STAT;
+    if (categoryCode==2) return LegacyType.CLASS;
+    if (categoryCode==3) return LegacyType.DPS;
+    if (categoryCode==4) return LegacyType.OUTGOING_HEALING;
+    if (categoryCode==5) return LegacyType.INCOMING_HEALING;
+    if (categoryCode==6) return LegacyType.TACTICAL_DPS;
+    return null;
   }
 
   private void doIt()
   {
     PropertiesSet props=_facade.loadProperties(1879108262+0x9000000);
-    //System.out.println(props.dump());
-    Object[] array=(Object[])props.getProperty("ItemAdvancement_WidgetDID_Array");
-    for(Object obj : array)
+    System.out.println(props.dump());
     {
-      // 327 items
-      int id=((Integer)obj).intValue();
-      loadLegacy(id);
+      Object[] array=(Object[])props.getProperty("ItemAdvancement_WidgetDID_Array");
+      for(Object obj : array)
+      {
+        // 327 items
+        int id=((Integer)obj).intValue();
+        loadLegacy(id);
+      }
     }
-
-    // ItemAdvancement_ImbuedLegacy_ClassicLegacyTransform gives pre-imbued->imbued transformation data
-    //ItemAdvancement_LegacyTypeName_Array: #1: 268460418 => old stat
-    // ItemAdvancement_ImbuedLegacy_Effect:
+    // Stat legacies (already included in the previous array)
     /*
-    System.out.println("ItemAdvancement_ImbuedLegacy_Effect:");
-    System.out.println(_facade.loadProperties(1879321627+0x9000000).dump());
-    => Effect
-        ID: 268460418, key=Skill_Beorning_IA_BearDamage, name=Bear form Damage, percentage
+    {
+      Object[] array=(Object[])props.getProperty("ItemAdvancement_ClassAgnosticWidgetDID_Array");
+      for(Object obj : array)
+      {
+        // 5 items
+        int id=((Integer)obj).intValue();
+        loadLegacy(id);
+      }
+    }
+    */
+    // Blank legacy (useless)
+    //int blank=((Integer)props.getProperty("ItemAdvancement_BlankImbuedWidgetDID")).intValue();
+    //loadLegacy(blank);
 
-  => same stat before and after imbuement.
-        */
+    // DPS legacies
+    {
+      Object[] array=(Object[])props.getProperty("ItemAdvancement_ImbuedDPSWidgetMap_Array");
+      for(Object obj : array)
+      {
+        // 2 items
+        PropertiesSet legacyProps=(PropertiesSet)obj;
+        int legacyId=((Integer)legacyProps.getProperty("ItemAdvancement_ImbuedDPSWidget")).intValue();
+        //int equipmentCategory=((Integer)legacyProps.getProperty("Item_EquipmentCategory")).intValue();
+        loadLegacy(legacyId);
+      }
+    }
 
     // ItemAdvancement_LegacyReplacement_Extraction_Array
     //System.out.println(_facade.loadProperties(1879319148+0x9000000).dump());
@@ -95,6 +176,7 @@ public class MainDatLegendariesExplorer
     System.out.println("DPS"); // Physical?
     System.out.println(_facade.loadProperties(1879325253+0x9000000).dump());
     */
+    System.out.println(statNames);
   }
 
   void doIt2()
@@ -149,7 +231,7 @@ public class MainDatLegendariesExplorer
   public static void main(String[] args)
   {
     DataFacade facade=new DataFacade();
-    new MainDatLegendariesExplorer(facade).doIt();
+    new MainDatLegaciesLoader(facade).doIt();
     facade.dispose();
   }
 }
