8b9b8adf9c9eb10024deb0e8d5170c2b1d2294c1
Damien Morcellet <dmorcellet@free.fr>
2019-11-21 22:02:02 +0100

    Added a tool to build all data files (added missing entry point...)

---
+++ src/main/java/delta/games/lotro/tools/dat/MainDatLoader.java
@@ -0,0 +1,201 @@
+package delta.games.lotro.tools.dat;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+
+import delta.common.utils.files.FilesDeleter;
+import delta.games.lotro.dat.data.DataFacade;
+import delta.games.lotro.tools.dat.characters.MainCharacterDataLoader;
+import delta.games.lotro.tools.dat.characters.SkillLoader;
+import delta.games.lotro.tools.dat.characters.TraitLoader;
+import delta.games.lotro.tools.dat.characters.VirtueDataLoader;
+import delta.games.lotro.tools.dat.combat.MainDatCombatLoader;
+import delta.games.lotro.tools.dat.crafting.MainDatCraftingLoader;
+import delta.games.lotro.tools.dat.crafting.MainDatRecipesLoader;
+import delta.games.lotro.tools.dat.emotes.MainDatEmotesLoader;
+import delta.games.lotro.tools.dat.factions.MainDatFactionsLoader;
+import delta.games.lotro.tools.dat.items.MainDatItemsLoader;
+import delta.games.lotro.tools.dat.items.MainDatItemsSetsLoader;
+import delta.games.lotro.tools.dat.items.legendary.LegaciesLoader;
+import delta.games.lotro.tools.dat.items.legendary.MainDatLegendaryTitlesLoader;
+import delta.games.lotro.tools.dat.misc.MainBuffsLoader;
+import delta.games.lotro.tools.dat.misc.MainDatColorLoader;
+import delta.games.lotro.tools.dat.misc.MainStatsLoader;
+import delta.games.lotro.tools.dat.quests.MainDatAchievablesLoader;
+import delta.games.lotro.tools.dat.relics.MainDatRelicsLoader;
+import delta.games.lotro.tools.dat.titles.MainDatTitlesLoader;
+import delta.games.lotro.tools.lore.traitPoints.TraitPointsRegistryBuilder;
+
+/**
+ * Global procedure to load data from DAT files.
+ * @author DAM
+ */
+public class MainDatLoader
+{
+  private static final Logger LOGGER=Logger.getLogger(MainDatLoader.class);
+
+  private DataFacade _facade;
+
+  /**
+   * Constructor.
+   * @param facade Data facade.
+   */
+  public MainDatLoader(DataFacade facade)
+  {
+    _facade=facade;
+  }
+
+  private void doIt()
+  {
+    cleanup();
+    load();
+  }
+
+  private void load()
+  {
+    // Stats
+    new MainStatsLoader(_facade).doIt();
+    // Colors
+    new MainDatColorLoader(_facade).doIt();
+    // Combat data
+    new MainDatCombatLoader(_facade).doIt();
+    new MainProgressionsMerger().doIt();
+    // Character data
+    new MainCharacterDataLoader(_facade).doIt();
+    new MainProgressionsMerger().doIt();
+    // Titles
+    new MainDatTitlesLoader(_facade).doIt();
+    // Items
+    new MainDatItemsLoader(_facade).doIt();
+    new MainProgressionsMerger().doIt();
+    // Items sets
+    new MainDatItemsSetsLoader(_facade).doIt();
+    // Legendary titles
+    new MainDatLegendaryTitlesLoader(_facade).doIt();
+    // Relics
+    new MainDatRelicsLoader(_facade).doIt();
+    // Crafting
+    new MainDatCraftingLoader(_facade).doIt();
+    // Recipes
+    new MainDatRecipesLoader(_facade).doIt();
+    // Emotes
+    new MainDatEmotesLoader(_facade).doIt();
+    // Factions
+    new MainDatFactionsLoader(_facade).doIt();
+    // Quests and deeds
+    new MainDatAchievablesLoader(_facade).doIt();
+    new MainProgressionsMerger().doIt();
+    // Factions (reloaded)
+    new MainDatFactionsLoader(_facade).doIt();
+    // Buffs
+    new MainBuffsLoader(_facade).doIt();
+    // Trait points
+    new TraitPointsRegistryBuilder().doIt();
+    // Merge progressions
+    new MainProgressionsMerger().doIt();
+  }
+
+  private void cleanup()
+  {
+    // Commons
+    deleteFile(GeneratedFiles.STATS);
+    deleteFile(GeneratedFiles.COLORS);
+    deleteFile(GeneratedFiles.COMBAT_DATA);
+    // Character data
+    deleteFile(GeneratedFiles.STAT_CONTRIBS);
+    deleteFile(GeneratedFiles.START_STATS);
+    deleteFile(GeneratedFiles.CLASSES);
+    deleteFile(GeneratedFiles.RACES);
+    // - skills
+    deleteFile(GeneratedFiles.SKILLS);
+    deleteFile(GeneratedFiles.SKILL_ICONS);
+    deleteDirectory(SkillLoader.SKILL_ICONS_DIR);
+    // - virtues
+    deleteFile(GeneratedFiles.VIRTUES);
+    deleteFile(GeneratedFiles.VIRTUE_ICONS);
+    deleteDirectory(VirtueDataLoader.VIRTUE_ICONS_DIR);
+    // - traits
+    deleteFile(GeneratedFiles.TRAITS);
+    deleteFile(GeneratedFiles.TRAIT_ICONS);
+    deleteDirectory(TraitLoader.TRAIT_ICONS_DIR);
+    // Titles
+    deleteFile(GeneratedFiles.TITLES);
+    deleteFile(GeneratedFiles.TITLE_ICONS);
+    deleteDirectory(MainDatTitlesLoader.TITLE_ICONS_DIR);
+    // Items
+    deleteFile(GeneratedFiles.ITEMS);
+    deleteDirectory(GeneratedFiles.ITEM_ICONS_DIR);
+    deleteFile(GeneratedFiles.PASSIVES);
+    deleteFile(GeneratedFiles.PASSIVES_USAGE);
+    deleteFile(GeneratedFiles.CONSUMABLES);
+    // - legacies
+    deleteFile(GeneratedFiles.LEGACIES);
+    deleteFile(GeneratedFiles.NON_IMBUED_LEGACIES);
+    deleteFile(GeneratedFiles.LEGACIES_ICONS);
+    deleteDirectory(LegaciesLoader.LEGACIES_ICONS_DIR);
+    // Items sets
+    deleteFile(GeneratedFiles.SETS);
+    // Legendary titles
+    deleteFile(GeneratedFiles.LEGENDARY_TITLES);
+    // Relics
+    deleteFile(GeneratedFiles.RELICS);
+    deleteFile(GeneratedFiles.RELIC_ICONS);
+    deleteDirectory(MainDatRelicsLoader.RELIC_ICONS_DIR);
+    // Recipes
+    deleteFile(GeneratedFiles.RECIPES);
+    // Emotes
+    deleteFile(GeneratedFiles.EMOTES);
+    deleteFile(GeneratedFiles.EMOTE_ICONS);
+    deleteDirectory(MainDatEmotesLoader.EMOTE_ICONS_DIR);
+    // Factions
+    deleteFile(GeneratedFiles.FACTIONS);
+    // Quests and deeds
+    deleteFile(GeneratedFiles.QUESTS);
+    deleteFile(GeneratedFiles.DEEDS);
+    // Crafting
+    deleteFile(GeneratedFiles.CRAFTING_DATA);
+    // Buffs
+    deleteFile(GeneratedFiles.BUFFS);
+    // Trait points
+    deleteFile(GeneratedFiles.TRAIT_POINTS);
+
+    // Progressions
+    deleteFile(GeneratedFiles.PROGRESSIONS_COMBAT);
+    deleteFile(GeneratedFiles.PROGRESSIONS_CHARACTERS);
+    deleteFile(GeneratedFiles.PROGRESSIONS_ITEMS);
+    deleteFile(GeneratedFiles.PROGRESSIONS_ACHIEVABLES);
+    deleteFile(GeneratedFiles.PROGRESSIONS_BUFFS);
+    deleteFile(GeneratedFiles.PROGRESSIONS);
+  }
+
+  private void deleteFile(File toDelete)
+  {
+    if (toDelete.exists())
+    {
+      boolean ok=toDelete.delete();
+      if (!ok)
+      {
+        LOGGER.warn("Could not delete file: "+toDelete);
+      }
+    }
+  }
+
+  private void deleteDirectory(File toDelete)
+  {
+    FilesDeleter deleter=new FilesDeleter(toDelete,null,true);
+    deleter.doIt();
+  }
+
+  /**
+   * Main method for this tool.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    DataFacade facade=new DataFacade();
+    new MainDatLoader(facade).doIt();
+    facade.dispose();
+  }
+
+}
