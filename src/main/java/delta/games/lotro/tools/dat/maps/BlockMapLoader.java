d8571e132a3d185222e7539836918467548e9ac6
Damien Morcellet <dmorcellet@free.fr>
2020-07-12 22:21:31 +0200

    Fixed the BlockMap loader

--- src/main/java/delta/games/lotro/tools/dat/maps/BlockMapLoader.java
+++ src/main/java/delta/games/lotro/tools/dat/maps/BlockMapLoader.java
@@ -2,6 +2,9 @@ package delta.games.lotro.tools.dat.maps;
 
 import java.io.ByteArrayInputStream;
 
+import delta.games.lotro.dat.DATFilesConstants;
+import delta.games.lotro.dat.archive.DATArchive;
+import delta.games.lotro.dat.archive.DatFilesManager;
 import delta.games.lotro.dat.data.DataFacade;
 import delta.games.lotro.dat.utils.BufferUtils;
 
@@ -30,26 +33,29 @@ public class BlockMapLoader
    */
   private void loadBlockMap(int region, int blockX, int blockY)
   {
-    int blockMapDID=0x80100000+(region*0x10000)+(blockX*0x100)+blockY;
+    long blockMapDID=0x80100000L+(region*0x10000)+(blockX*0x100)+blockY;
 
-    byte[] data=_facade.loadData(blockMapDID);
-    if (data!=null)
+    DatFilesManager datFilesMgr=_facade.getDatFilesManager();
+    DATArchive map=datFilesMgr.getArchive(DATFilesConstants.MAP_SEED+region);
+    byte[] data=map.loadEntry(blockMapDID);
+    if (data==null)
     {
-      System.out.println("*** Block map: region="+region+", blockX="+blockX+", blockY="+blockY);
-      ByteArrayInputStream bis=new ByteArrayInputStream(data);
-      int did=BufferUtils.readUInt32(bis);
-      if (did!=blockMapDID)
-      {
-        throw new IllegalArgumentException("Expected DID for block map: "+blockMapDID);
-      }
-      int count=BufferUtils.readTSize(bis);
-      System.out.println(count+" entries in this block map!");
-      for(int i=0;i<count;i++)
-      {
-        int key=BufferUtils.readUInt32(bis);
-        int index=BufferUtils.readUInt32(bis);
-        System.out.println("Mapping key="+key+" to index="+index);
-      }
+      return;
+    }
+    System.out.println("*** Block map: region="+region+", blockX="+blockX+", blockY="+blockY);
+    ByteArrayInputStream bis=new ByteArrayInputStream(data);
+    long did=BufferUtils.readUInt32AsLong(bis);
+    if (did!=blockMapDID)
+    {
+      throw new IllegalArgumentException("Expected DID for block map: "+blockMapDID);
+    }
+    int count=BufferUtils.readTSize(bis);
+    System.out.println(count+" entries in this block map!");
+    for(int i=0;i<count;i++)
+    {
+      int key=BufferUtils.readUInt32(bis);
+      int index=BufferUtils.readUInt32(bis);
+      System.out.println("Mapping key="+key+" to index="+index);
     }
   }
 
