e5673f32a1f2fcdd70f1d58a25365c25ffec8c51
Damien Morcellet <dmorcellet@free.fr>
2020-08-06 23:11:52 +0200

    Improved the map notes loader

--- src/main/java/delta/games/lotro/tools/dat/maps/MapNotesLoader.java
+++ src/main/java/delta/games/lotro/tools/dat/maps/MapNotesLoader.java
@@ -2,14 +2,19 @@ package delta.games.lotro.tools.dat.maps;
 
 import java.io.ByteArrayInputStream;
 import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import delta.common.utils.misc.IntegerHolder;
 import delta.games.lotro.common.Identifiable;
 import delta.games.lotro.dat.data.DatPosition;
 import delta.games.lotro.dat.data.DataFacade;
+import delta.games.lotro.dat.data.DataIdentification;
 import delta.games.lotro.dat.data.PropertiesSet;
 import delta.games.lotro.dat.data.PropertiesSet.PropertyValue;
+import delta.games.lotro.dat.data.enums.EnumMapper;
 import delta.games.lotro.dat.loaders.DBPropertiesLoader;
 import delta.games.lotro.dat.loaders.GeoLoader;
 import delta.games.lotro.dat.loaders.LoaderUtils;
@@ -17,6 +22,7 @@ import delta.games.lotro.dat.loaders.PositionDecoder;
 import delta.games.lotro.dat.loaders.PropertyUtils;
 import delta.games.lotro.dat.utils.BitSetUtils;
 import delta.games.lotro.dat.utils.BufferUtils;
+import delta.games.lotro.dat.utils.DataIdentificationTools;
 import delta.games.lotro.dat.utils.StringUtils;
 import delta.games.lotro.lore.maps.Area;
 import delta.games.lotro.lore.maps.Dungeon;
@@ -31,14 +37,16 @@ public class MapNotesLoader
 {
   private static final Logger LOGGER=Logger.getLogger(MapNotesLoader.class);
 
+  private static final boolean VERBOSE=false;
   private static final int MAP_NOTES_DID=0x0E000006;
 
   private DataFacade _facade;
-  //private EnumMapper _mapNoteType;
-  //private EnumMapper _mapLevel;
+  private EnumMapper _mapNoteType;
+  private EnumMapper _mapLevel;
   private DungeonLoader _dungeonLoader;
   private GeoAreasLoader _geoAreasLoader;
   private MapsDataManager _mapsDataManager;
+  private Map<String,IntegerHolder> _typesCount=new HashMap<String,IntegerHolder>();
 
   /**
    * Constructor.
@@ -51,95 +59,113 @@ public class MapNotesLoader
   {
     _facade=facade;
     _mapsDataManager=mapsDataManager;
-    //_mapNoteType=facade.getEnumsManager().getEnumMapper(587202775);
-    //_mapLevel=facade.getEnumsManager().getEnumMapper(587202774);
+    _mapNoteType=facade.getEnumsManager().getEnumMapper(587202775);
+    _mapLevel=facade.getEnumsManager().getEnumMapper(587202774);
     _dungeonLoader=dungeonLoader;
     _geoAreasLoader=geoAreasLoader;
   }
 
-  @SuppressWarnings("unused")
   private void loadMapNote(ByteArrayInputStream bis)
   {
-    //System.out.println("****** Map note:");
     DatPosition position=GeoLoader.readPosition(bis);
-    //System.out.println("Position: "+position);
+    Identifiable where=null;
     // Area ID
     int areaDID=BufferUtils.readUInt32(bis);
+    Identifiable area=null;
     if (areaDID!=0)
     {
-      /*Identifiable where=*/getAreaOrDungeon(areaDID);
-      //System.out.println("AreaID="+areaDID+" => "+where);
+      where=getAreaOrDungeon(areaDID);
+      area=where;
+    }
+    if (area==null)
+    {
+      LOGGER.warn("Area is null: ID="+areaDID);
     }
     // Dungeon ID
     int dungeonDID=BufferUtils.readUInt32(bis);
+    Identifiable dungeon=null;
     if (dungeonDID!=0)
     {
-      /*Dungeon dungeon=*/_dungeonLoader.getDungeon(dungeonDID);
-      //PropertiesSet dungeonProps=_facade.loadProperties(dungeonWStateID+DATConstants.DBPROPERTIES_OFFSET);
-      //System.out.println("DungeonID="+dungeonDID+" => "+dungeon.getName());
+      where=_dungeonLoader.getDungeon(dungeonDID);
+      dungeon=where;
     }
 
-    // Various depending on what the MapNote represents
-    int noteDID=BufferUtils.readUInt32(bis); // Vendor NPC, Item (Invisible Collision Waypoint), Landmark...
-    /*
+    // Associated data ID
+    int noteDID=BufferUtils.readUInt32(bis);
+    DataIdentification dataId=null;
     if (noteDID!=0)
     {
-      DataIdentification dataId=DataIdentificationTools.identify(_facade,noteDID);
-      System.out.println("Note: "+dataId);
+      // {Milestone=327, Landmark=2459, Waypoint=1308, DoorTemplate=874, IItem=857, Hotspot=267, NPCTemplate=4273}
+      dataId=DataIdentificationTools.identify(_facade,noteDID);
+      if (VERBOSE)
+      {
+        IntegerHolder counter=_typesCount.get(dataId.getWClassName());
+        if (counter==null)
+        {
+          counter=new IntegerHolder();
+          _typesCount.put(dataId.getWClassName(),counter);
+        }
+        counter.increment();
+      }
     }
-    */
 
+    // Properties
     DBPropertiesLoader propsLoader=new DBPropertiesLoader(_facade);
 
     // Get content layers properties (Object[] with Integers)
+    Object[] contentLayersArray=null;
     PropertyValue contentLayersProperties=propsLoader.decodeProperty(bis,false);
     if (contentLayersProperties!=null)
     {
-      /*
-      Object[] contentLayersArray=(Object[])contentLayersProperties.getValue();
-      if (contentLayersArray.length>0)
-      {
-        System.out.println("Content layers properties: "+contentLayersProperties);
-      }
-      */
+      contentLayersArray=(Object[])contentLayersProperties.getValue();
     }
-    // The display text for the MapNote:
+    // Displayed text
     Object stringinfo=PropertyUtils.readStringInfoProperty(bis);
     int[] key=(int[])stringinfo;
     String[] labelArray=_facade.getStringsManager().resolveStringInfo(key[0],key[1]);
     String text=StringUtils.stringArrayToString(labelArray);
-    /*
-    if ((text!=null) && (text.length()>0))
-    {
-      System.out.println("Text: "+text);
-    }
-    */
     // Icon
     int iconId=BufferUtils.readUInt32(bis);
-    /*
-    if (iconId!=0)
-    {
-      System.out.println("IconID: "+iconId);
-    }
-    */
     // Level
     int levelCode=BufferUtils.readUInt32(bis);
-    /*
-    if (levelCode!=0)
-    {
-      BitSet levelsSet=BitSetUtils.getBitSetFromFlags(levelCode);
-      System.out.println("Levels: "+levelCode+" => "+BitSetUtils.getStringFromBitSet(levelsSet,_mapLevel," / "));
-    }
-    */
     // Type
     long type=BufferUtils.readLong64(bis);
     // (padding)
     LoaderUtils.readAssert8(bis,0);
+
+    if (VERBOSE)
+    {
+      System.out.println("****** Map note:");
+      System.out.println("Position: "+position);
+      System.out.println("Area: "+area);
+      if (dungeon!=null)
+      {
+        System.out.println("Dungeon: "+dungeon);
+      }
+      System.out.println("Data ID: "+dataId);
+      if ((contentLayersArray!=null) && (contentLayersArray.length>0))
+      {
+        System.out.println("Content layers properties: "+contentLayersProperties);
+      }
+      if ((text!=null) && (text.length()>0))
+      {
+        System.out.println("Text: "+text);
+      }
+      if (iconId!=0)
+      {
+        System.out.println("IconID: "+iconId);
+      }
+      if (levelCode!=0)
+      {
+        BitSet levelsSet=BitSetUtils.getBitSetFromFlags(levelCode);
+        System.out.println("Levels: "+levelCode+" => "+BitSetUtils.getStringFromBitSet(levelsSet,_mapLevel," / "));
+      }
+    }
+
     if (type==1)
     {
       // Link
       DatPosition destPosition=GeoLoader.readPosition(bis);
-      //System.out.println("Link! Target position: "+destPosition);
       // Dest area: is a Dungeon (most of the time) or an Area
       int destAreaId=BufferUtils.readUInt32(bis);
       Identifiable destArea=getAreaOrDungeon(destAreaId);
@@ -147,7 +173,6 @@ public class MapNotesLoader
       {
         LOGGER.warn("Destination area not found: "+destAreaId);
       }
-      //System.out.println("Dest area: "+destArea);
       // Dest dungeon
       int destDungeonId=BufferUtils.readUInt32(bis);
       if (destDungeonId!=0) // Always 0
@@ -161,43 +186,72 @@ public class MapNotesLoader
         LOGGER.warn("Dest note ID: "+destNoteId);
       }
       BufferUtils.skip(bis,6); // Always 0
+      if (VERBOSE)
+      {
+        System.out.println("Link! Target position: "+destPosition);
+        System.out.println("Dest area: "+destArea);
+      }
     }
     else
     {
       BitSet typeSet=BitSetUtils.getBitSetFromFlags(type);
-      //String typeStr=BitSetUtils.getStringFromBitSet(typeSet,_mapNoteType," / ");
-      //System.out.println("Type: "+type+" => "+typeStr);
       float minRange=BufferUtils.readFloat(bis);
       float maxRange=BufferUtils.readFloat(bis);
-      /*
-      if ((minRange>0) || (maxRange>0))
-      {
-        System.out.println("Range: min="+minRange+", max="+maxRange);
-      }
-      */
       int discoverableMapNoteIndex=BufferUtils.readUInt32(bis);
-      /*
-      if (discoverableMapNoteIndex!=0)
-      {
-        System.out.println("discoverableMapNoteIndex="+discoverableMapNoteIndex);
-      }
-      */
       PropertiesSet gameSpecificProps=new PropertiesSet();
       propsLoader.decodeProperties(bis,gameSpecificProps);
-      /*
-      if (gameSpecificProps.getPropertyNames().size()>0)
+
+      if (VERBOSE)
       {
-        System.out.println("Game specific props: "+gameSpecificProps.dump());
+        String typeStr=BitSetUtils.getStringFromBitSet(typeSet,_mapNoteType," / ");
+        System.out.println("Type: "+type+" => "+typeStr);
+        if ((minRange>0) || (maxRange>0))
+        {
+          System.out.println("Range: min="+minRange+", max="+maxRange);
+        }
+        if (discoverableMapNoteIndex!=0)
+        {
+          System.out.println("discoverableMapNoteIndex="+discoverableMapNoteIndex);
+        }
+        if (gameSpecificProps.getPropertyNames().size()>0)
+        {
+          System.out.println("Game specific props: "+gameSpecificProps.dump());
+        }
       }
-      */
+
+      // Checks
+      int region=position.getRegion();
+      if ((region<1) || (region>4))
+      {
+        LOGGER.warn("Weird region value: "+region);
+      }
+      int instance=position.getInstance();
+      if (instance!=0)
+      {
+        LOGGER.warn("Instance is not 0 for position: "+position);
+      }
+      int cell=position.getCell();
+      if (cell!=0)
+      {
+        if (!(where instanceof Dungeon))
+        {
+          //LOGGER.warn("Cell="+cell+" while where="+where+" for position: "+position);
+        }
+      }
+      // Build marker
       Marker marker=new Marker();
       float[] lonLat=PositionDecoder.decodePosition(position.getBlockX(),position.getBlockY(),position.getPosition().getX(),position.getPosition().getY());
       GeoPoint geoPoint=new GeoPoint(lonLat[0],lonLat[1]);
+      marker.setDid(noteDID);
       marker.setPosition(geoPoint);
       marker.setLabel(text);
       int code=typeSet.nextSetBit(0)+1;
       marker.setCategoryCode(code);
       _mapsDataManager.registerMarker(dungeonDID,marker);
+    }
+    if (where==null)
+    {
+      LOGGER.warn("Unidentified geo entity! AreaID="+areaDID+", DungeonID="+dungeonDID);
     }
   }
 
@@ -213,7 +267,6 @@ public class MapNotesLoader
     {
       return area;
     }
-    LOGGER.warn("Unidentified geo entity: "+id);
     return null;
   }
 
@@ -248,6 +301,7 @@ public class MapNotesLoader
   public void doIt()
   {
     loadMapNotes();
+    System.out.println(_typesCount);
   }
 
   /**
