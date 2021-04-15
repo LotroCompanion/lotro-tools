ae783e0fcb684240851c2715da2505f7d86add4d
Damien Morcellet <dmorcellet@free.fr>
2021-04-15 13:38:31 +0200

    Added support for the updates manager

---
+++ src/main/java/delta/games/lotro/tools/updater/MainInitUpdatesSystem.java
@@ -0,0 +1,62 @@
+package delta.games.lotro.tools.updater;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import delta.updates.data.DirectoryDescription;
+import delta.updates.data.SoftwareDescription;
+import delta.updates.data.SoftwarePackageDescription;
+import delta.updates.data.Version;
+import delta.updates.tools.PackagesBuilder;
+import delta.updates.tools.ToolsConfig;
+import delta.updates.utils.DescriptionBuilder;
+
+/**
+ * Initializes the given directory so that it becomes manageable
+ * by the updates manager.
+ * @author DAM
+ */
+public class MainInitUpdatesSystem
+{
+  /**
+   * Main method for this test.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    String baseURL="http://localhost:8080/delta-web-genea-1.1-SNAPSHOT/app/";
+    File to=new File("d:/tmp/lc15-packages");
+    ToolsConfig config=new ToolsConfig(baseURL,to); 
+
+    File from=new File("d:/tmp/lc15");
+    DescriptionBuilder descriptionBuilder=new DescriptionBuilder();
+    DirectoryDescription description=(DirectoryDescription)descriptionBuilder.build(from);
+    description.removeEntry(".updates");
+    description.setName("");
+    PackagesBuilder builder=new PackagesBuilder(from,config);
+
+    List<SoftwarePackageDescription> packages=new ArrayList<SoftwarePackageDescription>();
+    int packageID=0;
+    // Data
+    DirectoryDescription dataDir=(DirectoryDescription)description.getEntryByName("data");
+    packages.add(builder.buildPackage(packageID,"data",dataDir));
+    packageID++;
+    // Lib
+    DirectoryDescription libDir=(DirectoryDescription)description.getEntryByName("lib");
+    packages.add(builder.buildPackage(packageID,"lib",libDir));
+    packageID++;
+    // Remaining
+    packages.add(builder.buildPackage(packageID,"main",description));
+    packageID++;
+
+    // Software
+    SoftwareDescription software=new SoftwareDescription(0);
+    software.setName("Lotro Companion");
+    software.setVersion(new Version(1500,"15.0.29.0.1"));
+    software.setContentsDescription("Version 15 of the famous tool for Lotro");
+    software.setDate(new Date().getTime());
+    builder.updateSoftware(software,packages);
+  }
+}
