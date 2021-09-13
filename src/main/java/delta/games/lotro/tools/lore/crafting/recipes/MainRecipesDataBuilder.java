3d47c19b13370b3dcd2398a7ff95b78015f42835
Damien Morcellet <dmorcellet@free.fr>
2021-09-13 09:10:48 +0200

    Some progress on the recipes data builder

--- src/main/java/delta/games/lotro/tools/lore/crafting/recipes/MainRecipesDataBuilder.java
+++ src/main/java/delta/games/lotro/tools/lore/crafting/recipes/MainRecipesDataBuilder.java
@@ -1,5 +1,7 @@
 package delta.games.lotro.tools.lore.crafting.recipes;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,6 +13,14 @@ import delta.games.lotro.lore.items.ContainersManager;
 import delta.games.lotro.lore.items.Item;
 import delta.games.lotro.lore.items.ItemsContainer;
 import delta.games.lotro.lore.items.ItemsManager;
+import delta.games.lotro.lore.trade.barter.BarterEntry;
+import delta.games.lotro.lore.trade.barter.BarterEntryElement;
+import delta.games.lotro.lore.trade.barter.BarterNpc;
+import delta.games.lotro.lore.trade.barter.BarterersManager;
+import delta.games.lotro.lore.trade.barter.ItemBarterEntryElement;
+import delta.games.lotro.lore.trade.vendor.ValuedItem;
+import delta.games.lotro.lore.trade.vendor.VendorNpc;
+import delta.games.lotro.lore.trade.vendor.VendorsManager;
 
 /**
  * Builder for recipes related data.
@@ -36,7 +46,23 @@ public class MainRecipesDataBuilder
   private void doIt()
   {
     Set<Integer> recipeScrollsIds=getRecipeItemIds();
+    Set<Integer> foundInContainers=findScrollsInContainers(recipeScrollsIds);
+    Set<Integer> sold=findInVendors(recipeScrollsIds);
+    Set<Integer> bartered=findInBarterers(recipeScrollsIds);
+    // TODO Quest rewards
+
+    Set<Integer> scrollsNotFound=new HashSet<Integer>(recipeScrollsIds);
+    scrollsNotFound.removeAll(foundInContainers);
+    scrollsNotFound.removeAll(sold);
+    scrollsNotFound.removeAll(bartered);
+    System.out.println("Scrolls not found:");
+    showList(scrollsNotFound);
+  }
+
+  private Set<Integer> findScrollsInContainers(Set<Integer> recipeScrollsIds)
+  {
     Set<Integer> foundScrolls=new HashSet<Integer>();
+    Set<Integer> scrollContainers=new HashSet<Integer>();
     ContainersManager containersMgr=ContainersManager.getInstance();
     List<Container> containers=containersMgr.getContainers();
     for(Container container : containers)
@@ -46,13 +72,85 @@ public class MainRecipesDataBuilder
         ItemsContainer itemsContainer=(ItemsContainer)container;
         Set<Integer> containedItems=itemsContainer.getItemIds();
         containedItems.retainAll(recipeScrollsIds);
+        if (containedItems.size()>0)
+        {
+          scrollContainers.add(Integer.valueOf(container.getIdentifier()));
+        }
         foundScrolls.addAll(containedItems);
       }
     }
-    ItemsManager itemsMgr=ItemsManager.getInstance();
-    for(Integer scrollId : foundScrolls)
+    System.out.println("Scrolls found in containers:");
+    showList(foundScrolls);
+    System.out.println("Scroll containers:");
+    showList(scrollContainers);
+    return foundScrolls;
+  }
+
+  private Set<Integer> findInVendors(Set<Integer> recipeScrollsIds)
+  {
+    Set<Integer> soldItems=findSoldItems();
+    soldItems.retainAll(recipeScrollsIds);
+    System.out.println("Sold scrolls:");
+    showList(soldItems);
+    return soldItems;
+  }
+
+  private Set<Integer> findSoldItems()
+  {
+    Set<Integer> itemIds=new HashSet<Integer>();
+    VendorsManager vendorsMgr=VendorsManager.getInstance();
+    List<VendorNpc> vendorNpcs=vendorsMgr.getAll();
+    for(VendorNpc vendor : vendorNpcs)
     {
-      Item item=itemsMgr.getItem(scrollId.intValue());
+      List<ValuedItem> soldItems=vendor.getItemsList();
+      for(ValuedItem soldItem : soldItems)
+      {
+        itemIds.add(Integer.valueOf(soldItem.getId()));
+      }
+    }
+    return itemIds;
+  }
+
+  private Set<Integer> findInBarterers(Set<Integer> recipeScrollsIds)
+  {
+    Set<Integer> barteredItems=findBarteredItems();
+    barteredItems.retainAll(recipeScrollsIds);
+    System.out.println("Bartered scrolls:");
+    showList(barteredItems);
+    return barteredItems;
+  }
+
+  private Set<Integer> findBarteredItems()
+  {
+    Set<Integer> itemIds=new HashSet<Integer>();
+    BarterersManager barterersMgr=BarterersManager.getInstance();
+    List<BarterNpc> barterNpcs=barterersMgr.getAll();
+    for(BarterNpc barterer : barterNpcs)
+    {
+      List<BarterEntry> entries=barterer.getEntries();
+      for(BarterEntry entry : entries)
+      {
+        BarterEntryElement toReceive=entry.getElementToReceive();
+        if (toReceive instanceof ItemBarterEntryElement)
+        {
+          ItemBarterEntryElement itemEntry=(ItemBarterEntryElement)toReceive;
+          int itemId=itemEntry.getItemProxy().getId();
+          itemIds.add(Integer.valueOf(itemId));
+        }
+      }
+    }
+    return itemIds;
+  }
+
+  private void showList(Set<Integer> ids)
+  {
+    System.out.println("Nb items: "+ids.size());
+    ItemsManager itemsMgr=ItemsManager.getInstance();
+    List<Integer> sortedIds=new ArrayList<Integer>(ids);
+    Collections.sort(sortedIds);
+    for(Integer id : sortedIds)
+    {
+      Item item=itemsMgr.getItem(id.intValue());
       System.out.println(item);
     }
   }
