787ce421dd7177104b75d6679c7b7d4ba0b63da3
Damien Morcellet <dmorcellet@free.fr>
2021-10-15 08:33:34 +0200

    Updated the update package builder for 17.5

--- src/main/java/delta/games/lotro/tools/updater/MainBuildUpdatePackage.java
+++ src/main/java/delta/games/lotro/tools/updater/MainBuildUpdatePackage.java
@@ -41,9 +41,9 @@ public class MainBuildUpdatePackage
     String baseURL="https://sourceforge.net/projects/lotrocompanion/files/16/${file}/download";
     File to=new File("d:/tmp/lc16-packages");
     ToolsConfig config=new ToolsConfig(baseURL,to); 
-    File from=new File("D:/shared/damien/dev/lotrocompanion/releases/17.0/LotRO Companion/app");
-    String packageName="patch17.0";
-    Version newVersion=new Version(1700,"17.0.30.2");
+    File from=new File("D:/shared/damien/dev/lotrocompanion/releases/17.5/LotRO Companion/app");
+    String packageName="patch17.5";
+    Version newVersion=new Version(1750,"17.5.30.3");
 
     // Build software description
     LocalDataManager local=new LocalDataManager(from);
