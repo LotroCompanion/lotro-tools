5db2a0a614de0790a85e5d07e8b7d1d7738ed09f
Damien Morcellet <dmorcellet@free.fr>
2022-01-02 18:33:44 +0100

    Added some missing item-granted mounts

--- src/main/java/delta/games/lotro/tools/dat/items/ItemDetailsLoader.java
+++ src/main/java/delta/games/lotro/tools/dat/items/ItemDetailsLoader.java
@@ -1,13 +1,17 @@
 package delta.games.lotro.tools.dat.items;
 
+import delta.games.lotro.character.races.RaceDescription;
+import delta.games.lotro.character.races.RacesManager;
 import delta.games.lotro.character.skills.SkillDescription;
 import delta.games.lotro.character.skills.SkillsManager;
 import delta.games.lotro.character.traits.TraitDescription;
 import delta.games.lotro.character.traits.TraitsManager;
+import delta.games.lotro.common.Race;
 import delta.games.lotro.dat.data.PropertiesSet;
 import delta.games.lotro.lore.items.Item;
 import delta.games.lotro.lore.items.details.GrantType;
 import delta.games.lotro.lore.items.details.GrantedElement;
+import delta.games.lotro.tools.dat.utils.DatEnumsUtils;
 
 /**
  * Loader for item details.
@@ -31,6 +35,7 @@ public class ItemDetailsLoader
     handleGrantedSkill(item,props,"Item_GrantedSkill",GrantType.SKILL);
     handleGrantedSkill(item,props,"Mount_SkillToGrantShort",GrantType.SHORT_MOUNT);
     handleGrantedSkill(item,props,"Mount_SkillToGrantTall",GrantType.TALL_MOUNT);
+    handleGrantedMountSkillArray(item,props);
   }
 
   private void handleGrantedSkill(Item item, PropertiesSet props, String propertyName, GrantType type)
@@ -42,6 +47,39 @@ public class ItemDetailsLoader
       if (skill!=null)
       {
         GrantedElement<SkillDescription> grantedElement=new GrantedElement<SkillDescription>(type,skill);
+        Item.addDetail(item,grantedElement);
+      }
+    }
+  }
+
+  private void handleGrantedMountSkillArray(Item item, PropertiesSet props)
+  {
+/*
+Mount_SkillToGrantRaceArray: 
+  #1: Mount_SkillToGrantRace_Entry 
+    Mount_SkillToGrant: 1879245852
+    Usage_RequiredRaceEntry: 81 (Hobbit[O])
+    ...
+*/
+    Object[] array=(Object[])props.getProperty("Mount_SkillToGrantRaceArray");
+    if (array==null)
+    {
+      return;
+    }
+    for(Object entryObj : array)
+    {
+      PropertiesSet entryProps=(PropertiesSet)entryObj;
+      int skillID=((Integer)entryProps.getProperty("Mount_SkillToGrant")).intValue();
+      SkillDescription skill=SkillsManager.getInstance().getSkill(skillID);
+      if (skill!=null)
+      {
+        int raceCode=((Integer)entryProps.getProperty("Usage_RequiredRaceEntry")).intValue();
+        Race race=DatEnumsUtils.getRaceFromRaceId(raceCode);
+        RacesManager racesMgr=RacesManager.getInstance();
+        RaceDescription raceDescription=racesMgr.getRaceDescription(race);
+        boolean tall=raceDescription.isTall();
+        GrantType grantType=tall?GrantType.TALL_MOUNT:GrantType.SHORT_MOUNT;
+        GrantedElement<SkillDescription> grantedElement=new GrantedElement<SkillDescription>(grantType,skill);
         Item.addDetail(item,grantedElement);
       }
     }
