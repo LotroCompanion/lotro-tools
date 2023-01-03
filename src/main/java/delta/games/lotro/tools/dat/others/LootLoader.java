3656d5a8a3e7f6f527539956ee155fc02896072f
Damien Morcellet <dmorcellet@free.fr>
2023-01-03 22:43:33 +0100

    Fixed loot probabilities for SoA

--- src/main/java/delta/games/lotro/tools/dat/others/LootLoader.java
+++ src/main/java/delta/games/lotro/tools/dat/others/LootLoader.java
@@ -26,6 +26,7 @@ import delta.games.lotro.common.treasure.TrophyList;
 import delta.games.lotro.common.treasure.TrophyListEntry;
 import delta.games.lotro.common.treasure.WeightedTreasureTable;
 import delta.games.lotro.common.treasure.WeightedTreasureTableEntry;
+import delta.games.lotro.config.LotroCoreConfig;
 import delta.games.lotro.dat.DATConstants;
 import delta.games.lotro.dat.data.DataFacade;
 import delta.games.lotro.dat.data.PropertiesSet;
@@ -70,6 +71,19 @@ public class LootLoader
 
   private void loadProbabilities()
   {
+    boolean isLive=LotroCoreConfig.isLive();
+    if (isLive)
+    {
+      loadProbabilitiesLive();
+    }
+    else
+    {
+      loadProbabilitiesSoA();
+    }
+  }
+
+  private void loadProbabilitiesLive()
+  {
     // LootGenControl:
     PropertiesSet properties=_facade.loadProperties(1879076022+DATConstants.DBPROPERTIES_OFFSET);
     if (properties==null)
@@ -85,6 +99,31 @@ public class LootLoader
       _probabilities.put(Integer.valueOf(code),Float.valueOf(percentage));
       LOGGER.debug("Probability is "+percentage*100+" for "+_dropFrequency.getString(code));
     }
+  }
+
+  private void loadProbabilitiesSoA()
+  {
+    _probabilities.put(Integer.valueOf(4),Float.valueOf(1f)); // Always
+    _probabilities.put(Integer.valueOf(9),Float.valueOf(0.7f)); // Common
+    _probabilities.put(Integer.valueOf(12),Float.valueOf(0.68f)); // Trophy
+    _probabilities.put(Integer.valueOf(10),Float.valueOf(0.6f)); // Frequent
+    _probabilities.put(Integer.valueOf(3),Float.valueOf(0.25f)); // Uncommon
+    _probabilities.put(Integer.valueOf(2),Float.valueOf(0.1f)); // Rare
+    _probabilities.put(Integer.valueOf(5),Float.valueOf(0.05f)); // UltraRare
+    _probabilities.put(Integer.valueOf(8),Float.valueOf(0.01f)); // ImpossiblyRare
+
+    _probabilities.put(Integer.valueOf(11),Float.valueOf(1f)); // EpicRaid
+
+    _probabilities.put(Integer.valueOf(16),Float.valueOf(0.75f)); // ReputationCommon
+    _probabilities.put(Integer.valueOf(15),Float.valueOf(0.5f)); // ReputationFrequent
+    _probabilities.put(Integer.valueOf(17),Float.valueOf(0.25f)); // ReputationUncommon
+    _probabilities.put(Integer.valueOf(14),Float.valueOf(0.04f)); // ReputationRare
+    _probabilities.put(Integer.valueOf(13),Float.valueOf(0.02f)); // ReputationVeryRare
+
+    _probabilities.put(Integer.valueOf(20),Float.valueOf(0.6f)); // BarterCommon
+    _probabilities.put(Integer.valueOf(22),Float.valueOf(0.4f)); // BarterFrequent
+    _probabilities.put(Integer.valueOf(19),Float.valueOf(0.2f)); // BarterUncommon
+    _probabilities.put(Integer.valueOf(21),Float.valueOf(0.1f)); // BarterRare
   }
 
   /**
@@ -567,6 +606,10 @@ public class LootLoader
   private float getProbability(int frequencyCode)
   {
     Float probability=_probabilities.get(Integer.valueOf(frequencyCode));
+    if (probability==null)
+    {
+      LOGGER.warn("Missing probability: frequencyCode="+frequencyCode);
+    }
     return (probability!=null)?probability.floatValue():0.0f;
   }
 }
