ba672454f76eee40feb7b3a2ac7533c98b2fd323
Damien Morcellet <dmorcellet@free.fr>
2023-01-09 22:05:35 +0100

    Fixed/improved the dat files index builder

--- src/main/java/delta/games/lotro/tools/reports/DATFilesIndexBuilder.java
+++ src/main/java/delta/games/lotro/tools/reports/DATFilesIndexBuilder.java
@@ -5,7 +5,7 @@ import java.io.FileFilter;
 import java.util.List;
 
 import delta.common.utils.files.FilesFinder;
-import delta.common.utils.files.filter.ExtensionPredicate;
+import delta.games.lotro.config.LotroCoreConfig;
 import delta.games.lotro.dat.archive.DATArchive;
 import delta.games.lotro.dat.archive.DATArchiveIndexBuilder;
 import delta.games.lotro.dat.utils.ClientPathUtils;
@@ -16,7 +16,7 @@ import delta.games.lotro.dat.utils.ClientPathUtils;
  */
 public class DATFilesIndexBuilder
 {
-  private static final File ROOT_DIR=new File("../lotro-companion-private-doc/dat/index");
+  private static final File ROOT_DIR=new File("../lotro-companion-private-doc/dat");
 
   private File findClientDir()
   {
@@ -38,8 +38,13 @@ public class DATFilesIndexBuilder
     DATArchiveIndexBuilder builder=new DATArchiveIndexBuilder();
     DATArchive archive=new DATArchive(datFile);
     archive.open();
-    String indexName=datFile.getName().replace(".dat",".index.txt");
-    File indexFile=new File(ROOT_DIR,indexName);
+    String name=datFile.getName();
+    name=name.substring(0,name.indexOf('.'));
+    boolean isLive=LotroCoreConfig.isLive();
+    File dir=new File(ROOT_DIR,isLive?"index":"index-soa");
+    String indexName=name+".index.txt";
+    File indexFile=new File(dir,indexName);
+    indexFile.getParentFile().mkdirs();
     builder.buildIndex(archive,indexFile);
     archive.close();
   }
@@ -51,7 +56,20 @@ public class DATFilesIndexBuilder
   {
     File clientDir=findClientDir();
     FilesFinder finder=new FilesFinder();
-    FileFilter filter=new ExtensionPredicate(".dat");
+    FileFilter filter=new FileFilter()
+    {
+      @Override
+      public boolean accept(File pathname)
+      {
+        String name=pathname.getName();
+        if ((name.startsWith("client_")) &&
+            ((name.endsWith(".dat"))||(name.endsWith(".datx"))))
+        {
+          return true;
+        }
+        return false;
+      }
+    };
     List<File> files=finder.find(FilesFinder.ABSOLUTE_MODE,clientDir,filter,false);
     for(File file : files)
     {
