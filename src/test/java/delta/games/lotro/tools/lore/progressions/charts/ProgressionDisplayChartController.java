701503ab5b03c62e46408bcfabdf648c229c34a0
Damien Morcellet <dmorcellet@free.fr>
2023-08-11 13:38:29 +0200

    Reuse a useless chart display tool to have progression charts

--- src/test/java/delta/games/lotro/tools/lore/progressions/charts/ProgressionDisplayChartController.java
+++ src/test/java/delta/games/lotro/tools/lore/progressions/charts/ProgressionDisplayChartController.java
@@ -1,4 +1,4 @@
-package delta.games.lotro.tools.lore.items.scalables.charts;
+package delta.games.lotro.tools.lore.progressions.charts;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -7,6 +7,7 @@ import java.awt.Paint;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -25,40 +26,38 @@ import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.RectangleEdge;
 
 import delta.common.ui.swing.GuiFactory;
-import delta.games.lotro.lore.items.ArmourType;
-import delta.games.lotro.lore.items.ArmourTypes;
-import delta.games.lotro.lore.items.EquipmentLocation;
-import delta.games.lotro.lore.items.EquipmentLocations;
-import delta.games.lotro.lore.items.ItemQualities;
-import delta.games.lotro.lore.items.ItemQuality;
+import delta.games.lotro.common.progression.ProgressionsManager;
+import delta.games.lotro.utils.maths.ArrayProgression;
+import delta.games.lotro.utils.maths.LinearInterpolatingProgression;
+import delta.games.lotro.utils.maths.Progression;
 
 /**
- * Controller for armour stat charts.
+ * Controller to display progression charts.
  * @author DAM
  */
-public class ScalableStatChartController
+public class ProgressionDisplayChartController
 {
   // GUI
   private JPanel _panel;
   private JFreeChart _chart;
-  private ArmourType _type;
-  private EquipmentLocation _location;
-  //private ScaledArmourComputer _computer;
+  // Data
+  private int _progressionID;
+  private String _name;
 
   /**
    * Constructor.
-   * @param type Armour type to use.
-   * @param location Armour location.
+   * @param progressionID Progression ID.
+   * @param name Name.
    */
-  public ScalableStatChartController(ArmourType type, EquipmentLocation location)
+  public ProgressionDisplayChartController(int progressionID, String name)
   {
-    _type=type;
-    _location=location;
-    //_computer=new ScaledArmourComputer();
+    _progressionID=progressionID;
+    _name=name;
   }
 
-  private String getTitle() {
-    String title=_type + " - " + _location;
+  private String getTitle()
+  {
+    String title=_name+" - "+_progressionID;
     return title;
   }
 
@@ -96,7 +95,7 @@ public class ScalableStatChartController
 
   private JFreeChart buildChart()
   {
-    String xAxisLabel="Item level";
+    String xAxisLabel="X";
     String yAxisLabel=getTitle();
     String title=yAxisLabel;
 
@@ -127,7 +126,7 @@ public class ScalableStatChartController
     };
     XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
     renderer.setSeriesLinesVisible(0, true);
-    renderer.setSeriesShapesVisible(0, true);
+    renderer.setSeriesShapesVisible(0, false);
     renderer.setBaseToolTipGenerator(tooltip);
     plot.setRenderer(renderer);
 
@@ -154,28 +153,40 @@ public class ScalableStatChartController
 
   private XYSeriesCollection createDataset()
   {
-    ItemQuality[] qualities={ ItemQualities.UNCOMMON, ItemQualities.RARE, ItemQualities.INCOMPARABLE, ItemQualities.LEGENDARY};
     XYSeriesCollection data=new XYSeriesCollection();
-    for(ItemQuality quality : qualities)
+    XYSeries toonSeries = new XYSeries(String.valueOf(_progressionID));
+    Progression progression=ProgressionsManager.getInstance().getProgression(_progressionID);
+    if (progression instanceof LinearInterpolatingProgression)
     {
-      addSeries(data,quality);
+      LinearInterpolatingProgression lip=(LinearInterpolatingProgression)progression;
+      int minX=lip.getMinX();
+      int maxX=lip.getMaxX();
+      for(int i=minX;i<maxX;i++)
+      {
+        double value=lip.getValue(i).floatValue();
+        toonSeries.add(i,value);
+      }
     }
-    return data;
-  }
-
-  private void addSeries(XYSeriesCollection data, ItemQuality quality)
-  {
-    String key=quality.getMeaning();
-    XYSeries toonSeries = new XYSeries(key);
-    // 99->105
-    int[] levels={ 188, 192, 202, 207, 212, 217 };
-    for(int i=0;i<levels.length;i++)
+    else if (progression instanceof ArrayProgression)
     {
-      int level=levels[i];
-      double value=1;/*_computer.getArmour(level,_type,_location,quality,1);*/
-      toonSeries.add(level,value);
+      ArrayProgression ap=(ArrayProgression)progression;
+      int nbPoints=ap.getNumberOfPoints();
+      int minX=Integer.MAX_VALUE;
+      int maxX=Integer.MIN_VALUE;
+      for(int i=0;i<nbPoints;i++)
+      {
+        int x=ap.getX(i);
+        if (minX>x) minX=x;
+        if (maxX<x) maxX=x;
+      }
+      for(int i=minX;i<maxX;i++)
+      {
+        float value=ap.getValue(i).floatValue();
+        toonSeries.add(i,value);
+      }
     }
     data.addSeries(toonSeries);
+    return data;
   }
 
   /**
@@ -197,24 +208,28 @@ public class ScalableStatChartController
    */
   public static void main(String[] args)
   {
-    ArmourType[] types={ArmourTypes.HEAVY, ArmourTypes.MEDIUM, ArmourTypes.LIGHT};
-    EquipmentLocation[] locations={EquipmentLocations.FEET, EquipmentLocations.LEGS, EquipmentLocations.CHEST,
-        EquipmentLocations.SHOULDER, EquipmentLocations.HEAD, EquipmentLocations.HAND, EquipmentLocations.BACK
+    int[] progressionIDs = new int[] {
+        1879426638,
+        1879055446
     };
-    for(ArmourType type : types)
+    String[] names = new String[] {
+        "HPS",
+        "Armour"
+    };
+    int i=0;
+    for(int progressionID : progressionIDs)
     {
-      for(EquipmentLocation location : locations)
-      {
-        JPanel panel=GuiFactory.buildBackgroundPanel(new BorderLayout());
-        ScalableStatChartController chartController=new ScalableStatChartController(type,location);
-        JPanel chartPanel=chartController.getPanel();
-        panel.add(chartPanel,BorderLayout.CENTER);
-        JFrame frame=new JFrame();
-        frame.setTitle(chartController.getTitle());
-        frame.getContentPane().add(chartPanel);
-        frame.pack();
-        frame.setVisible(true);
-      }
+      JPanel panel=GuiFactory.buildBackgroundPanel(new BorderLayout());
+      ProgressionDisplayChartController chartController=new ProgressionDisplayChartController(progressionID,names[i]);
+      JPanel chartPanel=chartController.getPanel();
+      panel.add(chartPanel,BorderLayout.CENTER);
+      JFrame frame=new JFrame();
+      frame.setTitle(chartController.getTitle());
+      frame.getContentPane().add(chartPanel);
+      frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+      frame.pack();
+      frame.setVisible(true);
+      i++;
     }
   }
 }
