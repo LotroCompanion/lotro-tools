c8fc75fe431adbcff8f18b9c843a6c80a150ed84
Damien Morcellet <dmorcellet@free.fr>
2024-06-26 22:13:18 +0200

    Adapt to changes in contextual rendering

--- src/main/java/delta/games/lotro/tools/reports/ReportUtils.java
+++ src/main/java/delta/games/lotro/tools/reports/ReportUtils.java
@@ -1,15 +1,12 @@
 package delta.games.lotro.tools.reports;
 
+import java.util.Map;
+
 import delta.common.utils.variables.VariablesResolver;
 import delta.games.lotro.character.BaseCharacterSummary;
-import delta.games.lotro.character.classes.ClassDescription;
-import delta.games.lotro.character.classes.ClassesManager;
-import delta.games.lotro.character.classes.WellKnownCharacterClassKeys;
-import delta.games.lotro.character.races.RaceDescription;
-import delta.games.lotro.character.races.RacesManager;
-import delta.games.lotro.common.Genders;
 import delta.games.lotro.dat.data.strings.renderer.StringRenderer;
 import delta.games.lotro.utils.strings.ContextVariableValueProvider;
+import delta.games.lotro.utils.strings.RenderingUtils;
 
 /**
  * Utility methods related to reports.
@@ -17,29 +14,7 @@ import delta.games.lotro.utils.strings.ContextVariableValueProvider;
  */
 public class ReportUtils
 {
-  private static final BaseCharacterSummary DEFAULT_SUMMARY=buildDefaultSummary();
-
-  private static BaseCharacterSummary buildDefaultSummary()
-  {
-    BaseCharacterSummary ret=new BaseCharacterSummary();
-    // Class
-    ClassDescription characterClass=ClassesManager.getInstance().getCharacterClassByKey(WellKnownCharacterClassKeys.CHAMPION);
-    ret.setCharacterClass(characterClass);
-    // Race
-    RaceDescription race=RacesManager.getInstance().getByKey("man");
-    ret.setRace(race);
-    // Gender
-    ret.setCharacterSex(Genders.MALE);
-    // Level
-    ret.setLevel(150);
-    // Name
-    ret.setName("(character name)"); // I18n
-    // Surname
-    ret.setSurname("(surname)"); // I18n
-    // Rank
-    ret.setRankCode(null);
-    return ret;
-  }
+  private static final BaseCharacterSummary DEFAULT_SUMMARY=RenderingUtils.buildDefaultSummary();
 
   /**
    * Build a renderer for reports.
@@ -47,8 +22,8 @@ public class ReportUtils
    */
   public static VariablesResolver buildRenderer()
   {
-    ContextVariableValueProvider provider=new ContextVariableValueProvider();
-    provider.setup(DEFAULT_SUMMARY);
+    Map<String,String> context=RenderingUtils.setupContext(DEFAULT_SUMMARY);
+    ContextVariableValueProvider provider=new ContextVariableValueProvider(context);
     StringRenderer renderer=new StringRenderer(provider);
     return renderer.getResolver();
   }
