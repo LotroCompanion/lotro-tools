2afff39d6cbcf79a68c781660834e90397b82ed2
Damien Morcellet <dmorcellet@free.fr>
2024-09-23 22:20:18 +0200

    Use JUnit 5 instead of JUnit 3

--- src/test/java/delta/games/lotro/tools/extraction/maps/finder/AdvancedMapsManagerTest.java
+++ src/test/java/delta/games/lotro/tools/extraction/maps/finder/AdvancedMapsManagerTest.java
@@ -1,25 +1,29 @@
 package delta.games.lotro.tools.extraction.maps.finder;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.io.File;
+
+import org.junit.jupiter.api.Test;
 
 import delta.games.lotro.lore.maps.ParchmentMapsManager;
 import delta.games.lotro.maps.data.MapsManager;
 import delta.games.lotro.maps.data.basemaps.GeoreferencedBasemapsManager;
 import delta.games.lotro.tools.extraction.geo.maps.finder.AdvancedMapsManager;
 import delta.games.lotro.tools.extraction.geo.maps.finder.DetailedMap;
-import junit.framework.Assert;
-import junit.framework.TestCase;
 
 /**
  * Test class for the advanced maps manager.
  * @author DAM
  */
-public class AdvancedMapsManagerTest extends TestCase
+class AdvancedMapsManagerTest
 {
   /**
    * Test the map finder.
    */
-  public void testBestMapForPoint()
+  @Test
+  void testBestMapForPoint()
   {
     File rootDir=new File("../lotro-maps-db");
     MapsManager mapsManager=new MapsManager(rootDir);
@@ -28,11 +32,11 @@ public class AdvancedMapsManagerTest extends TestCase
     AdvancedMapsManager mgr=new AdvancedMapsManager(mapsMgr,parchmentMapsMgr);
     // Floid and Dewitt position in Evendim
     DetailedMap map=mgr.getBestMapForPoint(1,-73.5065f,-14.199228f);
-    Assert.assertNotNull(map);
-    Assert.assertEquals(268439511,map.getIdentifier());
+    assertNotNull(map);
+    assertEquals(268439511,map.getIdentifier());
     // Prancing pony. This gives 2 maps, we expect the one of Bree and not the one of Bree-land 
     map=mgr.getBestMapForPoint(1,-51.7f,-30.6f);
-    Assert.assertNotNull(map);
-    Assert.assertEquals(268437716,map.getIdentifier());
+    assertNotNull(map);
+    assertEquals(268437716,map.getIdentifier());
   }
 }
