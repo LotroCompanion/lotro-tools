7e4762042ed11bd94db523463c21d41782a74fc9
Damien Morcellet <dmorcellet@free.fr>
2025-05-20 23:03:43 +0200

    Updated the distrib for version 2.0

--- tools/voicesExtractor/distrib/go.bat
+++ tools/voicesExtractor/distrib/go.bat
@@ -1,13 +1,15 @@
 
 @echo off
 
-set CLASSPATH=delta-common-1.10.jar
-set CLASSPATH=%CLASSPATH%;delta-json-1.1.1.jar
-set CLASSPATH=%CLASSPATH%;delta-lotro-dat-utils-10.2.jar
-set CLASSPATH=%CLASSPATH%;delta-voicesExtractor-1.0.jar
-set CLASSPATH=%CLASSPATH%;log4j-1.2.17.jar
+set CLASSPATH=delta-voicesExtractor-2.0.jar
+set CLASSPATH=%CLASSPATH%;delta-lotro-dat-utils-11.6.jar
+set CLASSPATH=%CLASSPATH%;delta-json-1.1.2.jar
+set CLASSPATH=%CLASSPATH%;delta-common-1.15.jar
+set CLASSPATH=%CLASSPATH%;slf4j-api-2.0.16.jar
+set CLASSPATH=%CLASSPATH%;logback-core-1.3.14.jar
+set CLASSPATH=%CLASSPATH%;logback-classic-1.3.14.jar
 
-set ROOT_DIR=d:\tmp\voix lotro
+set ROOT_DIR=d:\tmp\voix lotro2
 set LOTRO_CLIENT_PATH=C:\ProgramData\Turbine\The Lord of the Rings Online
 
 java delta.games.lotro.tools.voicesExtractor.MainVoicesExtractor --rootDir="%ROOT_DIR%" --language=fr
