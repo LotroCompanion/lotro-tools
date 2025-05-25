b482076d57041bfac4f7b5d9cd2b9039878cee6d
Damien Morcellet <dmorcellet@free.fr>
2025-05-25 22:26:53 +0200

    Updated the update package builder for 24.2.0.44.2

--- src/main/java/delta/games/lotro/tools/releng/lc/MainBuildUpdatePackage.java
+++ src/main/java/delta/games/lotro/tools/releng/lc/MainBuildUpdatePackage.java
@@ -41,8 +41,8 @@ public class MainBuildUpdatePackage
     File to=new File("d:/tmp/lc16-packages");
     ToolsConfig config=new ToolsConfig(baseURL,to);
     File from=new File("D:/shared/damien/dev/lotrocompanion/releases/work/LotRO Companion/app");
-    String packageName="patch24.1.0.44";
-    Version newVersion=new Version(24100,"24.1.0.44"); // NOSONAR (this is not an IP address)
+    String packageName="patch24.2.0.44.2";
+    Version newVersion=new Version(24200,"24.2.0.44.2"); // NOSONAR (this is not an IP address)
 
     // Build software description
     LocalDataManager local=new LocalDataManager(from);
