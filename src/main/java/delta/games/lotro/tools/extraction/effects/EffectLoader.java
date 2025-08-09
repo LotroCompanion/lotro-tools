27cf96a3764a13a4f9f1d8747163cb24c8148a79
Damien Morcellet <dmorcellet@free.fr>
2025-08-09 19:42:39 +0200

    Load details for "kill proc" effects

--- src/main/java/delta/games/lotro/tools/extraction/effects/EffectLoader.java
+++ src/main/java/delta/games/lotro/tools/extraction/effects/EffectLoader.java
@@ -33,6 +33,7 @@ import delta.games.lotro.common.effects.Hotspot;
 import delta.games.lotro.common.effects.InduceCombatStateEffect;
 import delta.games.lotro.common.effects.InstantFellowshipEffect;
 import delta.games.lotro.common.effects.InstantVitalEffect;
+import delta.games.lotro.common.effects.KillProcEffect;
 import delta.games.lotro.common.effects.PipEffect;
 import delta.games.lotro.common.effects.ProcEffect;
 import delta.games.lotro.common.effects.ProcEffectGenerator;
@@ -60,6 +61,7 @@ import delta.games.lotro.common.enums.PipAdjustmentType;
 import delta.games.lotro.common.enums.PipType;
 import delta.games.lotro.common.enums.ResistCategory;
 import delta.games.lotro.common.enums.SkillType;
+import delta.games.lotro.common.enums.SubSpecies;
 import delta.games.lotro.common.enums.VitalType;
 import delta.games.lotro.common.enums.VitalTypes;
 import delta.games.lotro.common.geo.ExtendedPosition;
@@ -104,6 +106,7 @@ public class EffectLoader
   private EffectsManager _effectsMgr;
   private PlacesLoader _placesLoader;
   private LotroEnum<AICooldownChannel> _cooldownChannelsEnum;
+  private LotroEnum<SubSpecies> _subSpeciesEnum;
 
   /**
    * Constructor.
@@ -118,6 +121,7 @@ public class EffectLoader
     _statUtils=new DatStatUtils(facade,_i18nUtils);
     _effectsMgr=EffectsManager.getInstance();
     _cooldownChannelsEnum=LotroEnumsRegistry.getInstance().get(AICooldownChannel.class);
+    _subSpeciesEnum=LotroEnumsRegistry.getInstance().get(SubSpecies.class);
   }
 
   /**
@@ -228,8 +232,7 @@ public class EffectLoader
         (classDef==753) || (classDef==739) || (classDef==748) ||
         (classDef==764) || (classDef==780) || (classDef==2156) ||
         (classDef==2259) || (classDef==2441) || (classDef==2459) ||
-        (classDef==3218) || (classDef==3690) || (classDef==3833) ||
-        (classDef==3842))
+        (classDef==3218) || (classDef==3690) || (classDef==3833))
     {
       return new PropertyModificationEffect();
     }
@@ -257,6 +260,7 @@ public class EffectLoader
     else if (classDef==718) return new FlagEffect();
     else if (classDef==763) return new AIPetEffect();
     else if (classDef==3184) return new CooldownEffect();
+    else if (classDef==3842) return new KillProcEffect();
     return new Effect();
   }
 
@@ -277,6 +281,10 @@ public class EffectLoader
     else if (effect instanceof CountDownEffect)
     {
       loadCountDownEffect((CountDownEffect)effect,effectProps);
+    }
+    else if (effect instanceof KillProcEffect)
+    {
+      loadKillProcEffect((KillProcEffect)effect,effectProps);
     }
     else if (effect instanceof PropertyModificationEffect)
     {
@@ -1577,6 +1585,80 @@ Effect_RandomEffect_Array:
         if (cooldownChannel!=null)
         {
           effect.addCooldownChannel(cooldownChannel);
+        }
+      }
+    }
+  }
+
+  private void loadKillProcEffect(KillProcEffect effect, PropertiesSet effectProps)
+  {
+    loadPropertyModificationEffect(effect,effectProps);
+    // 'caster' effects
+    Object[] casterEffectsList=(Object[])effectProps.getProperty("EffectGenerator_CasterEffectList");
+    if (casterEffectsList!=null)
+    {
+      for(Object entry : casterEffectsList)
+      {
+        PropertiesSet entryProps=(PropertiesSet)entry;
+        EffectGenerator generator=loadGenerator(entryProps);
+        effect.addCasterEffect(generator);
+      }
+    }
+    // 'user' effects
+    Object[] userEffectsList=(Object[])effectProps.getProperty("EffectGenerator_UserEffectList");
+    if (userEffectsList!=null)
+    {
+      for(Object entry : userEffectsList)
+      {
+        PropertiesSet entryProps=(PropertiesSet)entry;
+        EffectGenerator generator=loadGenerator(entryProps);
+        effect.addUserEffect(generator);
+      }
+    }
+    // Cooldown
+    // - base
+    float cooldown=((Float)effectProps.getProperty("Effect_KillProc_Cooldown")).floatValue();
+    effect.setCooldown(cooldown);
+    // - modifiers
+    ModPropertyList cooldownModifiers=ModifiersUtils.getStatModifiers(effectProps,"Effect_KillProc_Cooldown_ModifierList");
+    effect.setCooldownModifiers(cooldownModifiers);
+    // Proc Probability
+    // - base
+    Float procProbability=(Float)effectProps.getProperty("Effect_KillProc_ProcProbability");
+    if (procProbability!=null)
+    {
+      effect.setProbability(procProbability.floatValue());
+    }
+    // - modifiers
+    ModPropertyList procProbabilityModifiers=ModifiersUtils.getStatModifiers(effectProps,"Effect_KillProc_ProcProbability_ModifierList");
+    effect.setProbabilityModifiers(procProbabilityModifiers);
+    // Require kill shot
+    Integer requireKillShotInt=(Integer)effectProps.getProperty("Effect_KillProc_RequireKillShot");
+    boolean requireKillShot=((requireKillShotInt!=null) && (requireKillShotInt.intValue()==1));
+    effect.setRequiresKillShot(requireKillShot);
+    // On self killed
+    Integer onSelfKilledInt=(Integer)effectProps.getProperty("Effect_KillProc_OnSelfKilled");
+    boolean onSelfKilled=((onSelfKilledInt!=null) && (onSelfKilledInt.intValue()==1));
+    effect.setOnSelfKilled(onSelfKilled);
+    // Target required species
+    PropertyValue targetProperties=effectProps.getPropertyValueByName("Effect_KillProc_TargetRequiredProperties");
+    if (targetProperties!=null)
+    {
+      if (targetProperties instanceof ArrayPropertyValue)
+      {
+        ArrayPropertyValue arrayProps=(ArrayPropertyValue)targetProperties;
+        for(PropertyValue entry : arrayProps.getValues())
+        {
+          if ("Agent_Subspecies".equals(entry.getDefinition().getName()))
+          {
+            int speciesCode=((Integer)entry.getValue()).intValue();
+            SubSpecies species=_subSpeciesEnum.getEntry(speciesCode);
+            effect.setTargetRequiredSpecies(species);
+          }
+          else
+          {
+            LOGGER.warn("Unmanaged property value: {}",entry);
+          }
         }
       }
     }
