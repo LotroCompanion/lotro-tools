6bf2b7fdf126c5eec0c0c11c0861eba8ebc52346
Damien Morcellet <dmorcellet@free.fr>
2026-09-13 21:26:48 +0200

    Avoid noisy errors in the TraitKeyGenerator for EoA data loader

--- src/main/java/delta/games/lotro/tools/extraction/characters/TraitKeyGenerator.java
+++ src/main/java/delta/games/lotro/tools/extraction/characters/TraitKeyGenerator.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import delta.games.lotro.character.traits.TraitDescription;
 import delta.games.lotro.character.traits.TraitsManager;
+import delta.games.lotro.config.LotroCoreConfig;
 
 /**
  * Insert legacy keys into traits.
@@ -30,6 +31,7 @@ public class TraitKeyGenerator
    */
   public void setup()
   {
+    boolean isLive=LotroCoreConfig.isLive();
     // Man
     setup("BALANCE_OF_MAN",1879073534); // Balance of Man
     setup("MAN_OF_THE_FOURTH_AGE",1879073523); // Man of the Fourth Age
@@ -38,40 +40,42 @@ public class TraitKeyGenerator
     setup("FATEFUL_DWARF",1879073603); // Fateful Dwarf
     // Elf
     setup("FRIEND_OF_MAN",1879073565); // Friend Of Man
-    // Beorning
-    setup("EMISSARY",1879316867); // Emissary
     // Hobbit
     setup("HOBBIT_STATURE",1879073477); // Hobbit-stature
-    // High-elf
-    setup("THOSE_WHO_REMAIN",1879346315); // Those Who Remain
 
-    // Captain
-    setup("ARTERIAL_STRIKES",1879269756); // Arterial Strikes
-    setup("MARTIAL_PROWESS",1879269739); // Martial Prowess
-    setup("STEELED_RESOLVE",1879269738); // Steeled Resolve
-    // Champion
-    setup("CRIT_CHANCE_INCREASE",1879270411);
-    setup("MIGHT_INCREASE",1879270409);
-    setup("FINESSE_INCREASE",1879270418);
-    // Guardian
-    setup("OVERPOWER",1879272892); // Valorous Strength
-    setup("HEAVY_BLOWS",1879272894); // Heavy Blows
-    // Hunter
-    setup("CRITICAL_EYE",1879259548);
-    setup("IMPACT_ARROWS",1879264321); // Impact Arrows
-    // Lore-master
-    setup("TACTICAL_DAMAGE",1879269802); // Tactical Damage
-    // Minstrel
-    setup("ENDURING_MORALE",1879270745); // Enduring Morale
-    setup("FINESSE_MINSTREL",1879270760);
-    setup("CRITICAL_STRIKES",1879270774); // Critical Strikes
-    // Rune-keeper
-    setup("EXACTING_WARDS",1879270438); // Exacting Wards
-    setup("FORTUNE_SMILES",1879270465); // Fortune Smiles
-    setup("CUTTING_REMARKS",1879270469); // Cutting Remarks
-    setup("DETERMINATION",1879270470);
-    setup("LIGHT_ON_ONES_FEET",1879270461); // Light on One's Feet
-    setup("DELIBERATE_ADDRESS",1879270449); // Deliberate Address
+    if (isLive) {
+      // Beorning
+      setup("EMISSARY",1879316867); // Emissary
+      // High-elf
+      setup("THOSE_WHO_REMAIN",1879346315); // Those Who Remain
+      // Captain
+      setup("ARTERIAL_STRIKES",1879269756); // Arterial Strikes
+      setup("MARTIAL_PROWESS",1879269739); // Martial Prowess
+      setup("STEELED_RESOLVE",1879269738); // Steeled Resolve
+      // Champion
+      setup("CRIT_CHANCE_INCREASE",1879270411);
+      setup("MIGHT_INCREASE",1879270409);
+      setup("FINESSE_INCREASE",1879270418);
+      // Guardian
+      setup("OVERPOWER",1879272892); // Valorous Strength
+      setup("HEAVY_BLOWS",1879272894); // Heavy Blows
+      // Hunter
+      setup("CRITICAL_EYE",1879259548);
+      setup("IMPACT_ARROWS",1879264321); // Impact Arrows
+      // Lore-master
+      setup("TACTICAL_DAMAGE",1879269802); // Tactical Damage
+      // Minstrel
+      setup("ENDURING_MORALE",1879270745); // Enduring Morale
+      setup("FINESSE_MINSTREL",1879270760);
+      setup("CRITICAL_STRIKES",1879270774); // Critical Strikes
+      // Rune-keeper
+      setup("EXACTING_WARDS",1879270438); // Exacting Wards
+      setup("FORTUNE_SMILES",1879270465); // Fortune Smiles
+      setup("CUTTING_REMARKS",1879270469); // Cutting Remarks
+      setup("DETERMINATION",1879270470);
+      setup("LIGHT_ON_ONES_FEET",1879270461); // Light on One's Feet
+      setup("DELIBERATE_ADDRESS",1879270449); // Deliberate Address
+    }
   }
 
   private void setup(String key, int identifier)
